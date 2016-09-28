791df4d75f2fd88c8e42aa70105cbf47260c58c8
Nick Rumbaugh <takkyon13@gmail.com>
2016-09-28 13:49:15 -0700

    9.28.16.1547

--- run.9.28.16.1430.py
+++ run.9.28.16.1430.py
@@ -7,6 +7,6 @@ cr=np.loadtxt("Downloads/milliquas.txt",usecols=(0,1))
 crout=np.zeros((np.shape(cr)[0],3))
 
 crout[:,:2]=cr
-crout[:,2]=hp.ang2pix(nside,(90-cr[:,2])*np.pi/180.,cr[:,1]*np.pi/180,True)
+crout[:,2]=hp.ang2pix(nside,(90-cr[:,1])*np.pi/180.,cr[:,0]*np.pi/180,True)
 
 np.savetxt('/home/rumbaugh/milliquas+healpix.txt',crout,fmt='%f %f %i')
