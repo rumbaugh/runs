a64c3039b1cc5abfe911cd6f036f7e98e3b1b8c5
Nick Rumbaugh <takkyon13@gmail.com>
2016-10-07 14:11:15 -0700

    10.7.16.1611

---
+++ run.10.6.16.1420.py
@@ -0,0 +1,7 @@
+import numpy as np
+cr=np.loadtxt('milliquas_y1a1_match.tab',dtype={'names':('MQ','ra','dec','hpix','cid'),'formats':('i8','f8','f8','i8','i8')},skiprows=1)
+outcr=cr[cr['cid']!=0]
+np.savetxt('/home/rumbaugh/milliquas_y1a1_match_only.tab',outcr,fmt='%i %f %f %i %i',header='MQ_ROWNUM RA DEC HPIX COADD_OBJECTS_ID',comments='')
+outcr2=np.zeros((len(outcr),3))
+outcr2[:,0],outcr2[:,1],outcr2[:,2]=np.arange(len(outcr)),outcr['ra'],outcr['dec']
+np.savetxt('/home/rumbaugh/MQ_radec_monly.tab',outcr2,fmt='%i %f %f',header='NUMROW RA DEC',comments='')
