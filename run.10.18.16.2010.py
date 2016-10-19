c50b6be2587d9d49b5fa35a26c4aab815127ca06
Nick Rumbaugh <takkyon13@gmail.com>
2016-10-19 15:55:40 -0700

    10.19.16.1755

--- run.10.18.16.2010.py
+++ run.10.18.16.2010.py
@@ -1,7 +1,10 @@
 import numpy as np
 cr=np.loadtxt('milliquas_y1a1_match_pass2.tab',dtype={'names':('MQ','ra','dec','hpix','cid'),'formats':('i8','f8','f8','i8','i8')},skiprows=1)
-outcr=cr[cr['cid']!=0]
-np.savetxt('/home/rumbaugh/milliquas_y1a1_match_only.tab',outcr,fmt='%i %f %f %i %i',header='MQ_ROWNUM RA DEC HPIX COADD_OBJECTS_ID',comments='')
+#outcr=cr[cr['cid']!=0]
+cr=cr[cr['cid']!=0]
+outcr=np.zeros(len(cr),dtype=[('numrow', '<i8'),('MQ', '<i8'), ('ra', '<f8'), ('dec', '<f8'), ('hpix', '<i8'), ('cid', '<i8')])
+outcr['numrow'],outcr['MQ'],outcr['ra'],outcr['dec'],outcr['hpix'],outcr['cid']=np.arange(len(outcr)),cr['MQ'],cr['ra'],cr['dec'],cr['hpix'],cr['cid']
+np.savetxt('/home/rumbaugh/milliquas_y1a1_match_only.tab',outcr,fmt='%i %i %f %f %i %i',header='NUMROW MQ_ROWNUM RA DEC HPIX COADD_OBJECTS_ID',comments='')
 outcr2=np.zeros((len(outcr),3))
 outcr2[:,0],outcr2[:,1],outcr2[:,2]=np.arange(len(outcr)),outcr['ra'],outcr['dec']
-np.savetxt('/home/rumbaugh/MQ_radec_monly.tab',outcr2,fmt='%i %f %f',header='NUMROW RA DEC',comments='')
+np.savetxt('/home/rumbaugh/MQ_radec_monly.tab',outcr2,fmt='%i %i %f %f',header='NUMROW MQ_ROWNUM RA DEC',comments='')
