188dcc7790333135ab95ae885245a59e93f0c5f8
Nick Rumbaugh <takkyon13@gmail.com>
2016-10-21 16:13:58 -0700

    Fri Oct 21 16:13:58 PDT 2016

--- run.10.20.16.1045.py
+++ run.10.20.16.1045.py
@@ -1,5 +1,7 @@
 import easyaccess as ea
 import numpy as np
+
+yt='aaa'
 cr=np.loadtxt('/home/rumbaugh/milliquas_lightcurve_entries_y1a1.tab',skiprows=1,dtype={'names':('mjd','imageid','cid','MGid','ra','dec','mag','magerr','band','exp'),'formats':('f8','i8','i8','i8','f8','f8','f8','f8','|S12','f8')})
 IDs=np.unique(cr['cid'])
 exps=np.zeros(len(IDs))
