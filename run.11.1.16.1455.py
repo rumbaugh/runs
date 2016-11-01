cdf1c1d74930ce1de14a955c33ef2d9c94b320f3
Nick Rumbaugh <takkyon13@gmail.com>
2016-11-01 12:59:01 -0700

    vingilot - Tue Nov  1 12:59:01 PDT 2016

--- run.11.1.16.1455.py
+++ run.11.1.16.1455.py
@@ -15,19 +15,20 @@ outcr=np.zeros((maxDBID+1,15))
 outcr[:,0]=np.arange(0,maxDBID+1)
 for i in range(0,maxDBID+1):
     cr=np.loadtxt('/home/rumbaugh/var_database/%i/LC.tab'%i,dtype={'names':('DBID','Survey','CoaddID','ObjID','RA','DEC','MJD','TAG','BAND','MAGTYPE','MAG','MAGERR','Flag'),'formats':('i8','|S6','i8','i8','f8','f8','f8','|S12','|S4','|S8','f8','f8','i8')},skiprows=1)
-    gDES,gSDSS=np.where(cr['Survey']=='DES')[0],np.where(cr['Survey']=='SDSS')[0]
-    outcr[i][1]=cr['CoaddID'][gDES][0]
-    for ib,band in zip(np.arange(len(bands)),bands):
-        gb=np.where(cr['BAND'][gDES]==band)[0]
-        if len(gb)>0:
-            outcr[i][3+ib]=np.median(cr['MAG'][gDES][gb])
-    if len(gSDSS)>0: 
-        outcr[i][2]=cr['CoaddID'][gSDSS][0]
+    if np.shape(cr)!=():
+        gDES,gSDSS=np.where(cr['Survey']=='DES')[0],np.where(cr['Survey']=='SDSS')[0]
+        outcr[i][1]=cr['CoaddID'][gDES][0]
         for ib,band in zip(np.arange(len(bands)),bands):
-            gb=np.where(cr['BAND'][gSDSS]==band)[0]
+            gb=np.where(cr['BAND'][gDES]==band)[0]
             if len(gb)>0:
-                outcr[i][7+ib]=np.median(cr['MAG'][gSDSS][gb])
-                if outcr[i][3+ib]!=0:
-                    outcr[11+ib]=outcr[i][3+ib]-outcr[i][7+ib]
+                outcr[i][3+ib]=np.median(cr['MAG'][gDES][gb])
+        if len(gSDSS)>0: 
+            outcr[i][2]=cr['CoaddID'][gSDSS][0]
+            for ib,band in zip(np.arange(len(bands)),bands):
+                gb=np.where(cr['BAND'][gSDSS]==band)[0]
+                if len(gb)>0:
+                    outcr[i][7+ib]=np.median(cr['MAG'][gSDSS][gb])
+                    if outcr[i][3+ib]!=0:
+                        outcr[11+ib]=outcr[i][3+ib]-outcr[i][7+ib]
 np.savetxt('/home/rumbaugh/var_database/mag_changes.SDSS-DESS.dat',outcr,fmt='%5i %15i %15i %7.3f %7.3f %7.3f %7.3f %7.3f %7.3f %7.3f %7.3f %7.3f %7.3f %7.3f %7.3f',header='DBID COADD_OBJECTS_ID THINGID DES_g DES_r DES_i DES_z SDSS_g SDSS_r SDSS_i SDSS_z MAGDIFF_g MAGDIFF_r MAGDIFF_i MAGDIFF_z',comments='')
     
