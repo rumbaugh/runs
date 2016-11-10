ff21862d46e1018890a5cf50de64bfae679c0457
Nick Rumbaugh <takkyon13@gmail.com>
2016-11-09 17:29:31 -0800

    vingilot - Wed Nov  9 17:29:31 PST 2016

--- run.11.9.16.1900.py
+++ run.11.9.16.1900.py
@@ -16,11 +16,11 @@ for DBID in np.arange(0,numrows):
     db_cr=np.loadtxt('%s/%i/LC.tab'%(outputdir,DBID),dtype={'names':('DatabaseID','Survey','SurveyCoaddID','SurveyObjectID','RA','DEC','MJD','TAG','BAND','MAGTYPE','MAG','MAGERR','FLAG'),'formats':('i8','|S20','|S20','|S20','f8','f8','f8','|S20','|S12','|S12','f8','f8','i8')},skiprows=1)
     gDES,gSDSS=np.where(db_cr['Survey']=='DES')[0],np.where(db_cr['Survey']=='SDSS')[0]
     if len(gDES)>0:
-        cid=int(np.array([db_cr['SurveyCoaddID']])[gDES[0]])
+        cid=int(np.array(db_cr['SurveyCoaddID'])[gDES[0]])
     else:
         cid=0
     if len(gSDSS)>0:
-        tid=int(np.array([db_cr['SurveyCoaddID']])[gSDSS[0]])
+        tid=int(np.array(db_cr['SurveyCoaddID'])[gSDSS[0]])
     else:
         tid=0
     outcr[DBID]=[DBID,cid,tid]
