7321e3f5cc8f38beab37539fca4dff5b9f22c0b8
Nick Rumbaugh <takkyon13@gmail.com>
2016-11-10 08:46:26 -0800

    vingilot - Thu Nov 10 08:46:26 PST 2016

--- run.11.9.16.1940.py
+++ run.11.9.16.1940.py
@@ -15,6 +15,8 @@ cry=np.loadtxt('SDSSPOSS_lightcurve_entries_Y1A1.tab',skiprows=1,dtype={'names':
 
 crdb=np.loadtxt('/home/rumbaugh/var_database/database_index.dat')
 maxdbid=len(crdb)
+dbi_out=np.zeros((maxdbid,4),dtype='i8')
+dbi_out[:,:3]=np.copy(crdb)
 for curid in IDs[:10]:
     gid,gidy=np.where(cr['cid']==curid)[0],np.where(cry['cid']==curid)[0]
     mjd,mag,magerr,cID,bands,yra,ydec=cry['mjd'][gidy],cry['mag'][gidy],cry['magerr'][gidy],cry['cid'][gidy],cry['band'][gidy],cry['ra'][gidy],cry['dec'][gidy]
@@ -37,6 +39,7 @@ for curid in IDs[:10]:
     if cID in crdb[:,1]:
         gdb=np.where(cID==crdb[:,1])[0]
         DBID=crdb[gdb[0]]
+        dbi_out[gdb[0]][3]=cr['sdr7id'][gid][0]
         crLC=np.loadtxt('%s/%i/LC.tab'%(DB_path,DBID),dtype={'names':('DatabaseID','Survey','SurveyCoaddID','SurveyObjectID','RA','DEC','MJD','TAG','BAND','MAGTYPE','MAG','MAGERR','FLAG'),'formats':('i8','|S20','|S20','|S20','f8','f8','f8','|S20','|S20','|S20','f8','f8','i8')},skiprows=1)
         outcr=np.append(crLC,outcr)
         np.savetxt('%s/%i/LC.tab'%(DB_path,DBID),outcr,fmt=('%12i %20s %20s %20s %f %f %f %20s %12s %12s %f %f %i'),header=('DatabaseID Survey SurveyCoaddID SurveyObjectID RA DEC MJD TAG BAND MAGTYPE MAG MAGERR FLAG'),comments='')
@@ -49,4 +52,6 @@ for curid in IDs[:10]:
         np.savetxt('%s/%i/LC.tab'%(DB_path,DBID),outcr,fmt=('%12i %20s %20s %20s %f %f %f %20s %12s %12s %f %f %i'),header=('DatabaseID Survey SurveyCoaddID SurveyObjectID RA DEC MJD TAG BAND MAGTYPE MAG MAGERR FLAG'),comments='')
         np.savetxt('%s/%i/DES_data.tab'%(DB_path,DBID),db_outcr,fmt='%f %i %i %i %f %f %f %f %f %f %s %f',header=('MJD IMAGEID OBJECTID COADD_OBJECTS_ID RA DEC MAG_AUTO MAGERR_AUTO MAG_PSF MAGERR_PSF BAND EXPTIME'),comments='')
         #np.savetxt('%s/%i/SDSS_data.tab'%(DB_path,DBID),np.array(SDF)[gsci],fmt=('%f %f %f %i %i %f %f %f %f %f %f %f %f %f %f%i %i %i %i %i %i %i'),header=('MJD RA DEC OBJID NUMROW PSFMAG_U  PSFMAG_G  PSFMAG_R  PSFMAG_I  PSFMAG_Z  PSFMAGERR_U  PSFMAGERR_G  PSFMAGERR_R  PSFMAGERR_I  PSFMAGERR_Z RUN RERUN STRIPE THINGID MQ_ROWNUM COADD_OBJECTS_UD HPIX'),comments='')
+        dbi_out=np.append(dbi_out,np.array([DBID,curid,0,cr['sdr7id'][gid][0]]))
     print DBID, curid
+np.savetxt('/home/rumbaugh/var_database/database_index.dat',dbi_out,fmt='%i %i %i %i',header='DatabaseID Y1A1_COADD_OBJECTS_ID SDSS_DR13_thindid SDR7ID')
