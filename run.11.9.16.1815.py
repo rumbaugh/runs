f7269994a77928454c5de7f0939be71f9ec1acd2
Nick Rumbaugh <takkyon13@gmail.com>
2016-11-09 16:32:57 -0800

    vingilot - Wed Nov  9 16:32:57 PST 2016

--- run.11.9.16.1815.py
+++ run.11.9.16.1815.py
@@ -75,7 +75,14 @@ def plot_lightcurve(cid,band='all',plotSDSS=False,fname=None,connectpoints=True)
     plt.fontsize = 14
     plt.tick_params(which='major',length=8,width=2,labelsize=14)
     plt.tick_params(which='minor',length=4,width=1.5,labelsize=14)
-    ax=fig.add_subplot(1,1,2)
+    ax=fig.add_subplot(2,1,2)
+    if band=='all':
+        for b in coldict.keys():
+            plot_band(ax,g,b,connectpoints=connectpoints)
+        xlim=plt.xlim()
+        plt.xlim(xlim[0],xlim[1]+0.33*(xlim[1]-xlim[0]))
+        plt.legend()
+    ax=fig.add_subplot(2,1,1)
     if band=='all':
         for b in coldict.keys():
             plot_band(ax,g,b,connectpoints=connectpoints)
@@ -87,7 +94,7 @@ def plot_lightcurve(cid,band='all',plotSDSS=False,fname=None,connectpoints=True)
         plt.xlim(xlim[0],xlim[1]+0.33*(xlim[1]-xlim[0]))
         plt.legend()
     else:
-        plot_band(g,band,connectpoints=connectpoints)
+        plot_band(ax,g,band,connectpoints=connectpoints)
     ax.set_xlabel('MJD')
     ax.set_ylabel('Mag_PSF')
     ax.set_title(cid)
