05ffe5e4527746ade2079f2e5524fdc668d1ff5f
Nick Rumbaugh <takkyon13@gmail.com>
2016-11-16 09:45:17 -0800

    vingilot - Wed Nov 16 09:45:17 PST 2016

---
+++ run.11.16.16.1145.sql
@@ -0,0 +1,5 @@
+SELECT y.SP_ROWNUM,y.RA,y.DEC,t.TILENAME
+from RUMBAUGH.MILLIQUAS_Y1A1_MATCH y,RUMBAUGH.MILLIQUAS_Y1A1_TILES t
+where y.sp_rownum=t.sp_rownum
+and y.coadd_objects_id=0
+and t.TILENAME!='None'; >MILLIQUAS_INY1A1TILE.tab
