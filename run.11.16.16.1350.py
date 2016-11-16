2e9778ac08ec10570e9de52be8c87e17417e9d1e
Nick Rumbaugh <takkyon13@gmail.com>
2016-11-16 13:32:23 -0800

    vingilot - Wed Nov 16 13:32:23 PST 2016

---
+++ run.11.16.16.1350.py
@@ -0,0 +1,12 @@
+import numpy as np
+import angconvert as ang
+
+cr = np.loadtxt('/home/rumbaugh/MILLIQUAS_INY1A1TILE.csv',skiprows=1,dtype={'names':('NUMROW','MQ_ROWNUM','RA','DEC','TILENAME'),'formats':('i8','i8','f8','f8','|S30')})
+
+rah,ram,ras=ang.deg2hms(cr['RA'])
+decd,decm,decs=ang.deg2dms(cr['DEC'])
+crout=np.zeros(len(cr),dtype={'names':('NUMROW','MQ_ROWNUM','RA','DEC','OBJNAME','TILENAME'),'formats':('i8','i8','f8','f8','|S30','|S30')})
+crout['NUMROW'],crout['MQ_ROWNUM'],crout['RA'],crout['DEC'],crout['TILENAME']=cr['NUMROW'],cr['MQ_ROWNUM'],cr['RA'],cr['DEC'],cr['TILENAME']
+for i in range(0,len(cr)):
+    crout['OBJNAME'][i]='DESJ%02i%02i%04.1f%+03i%02i%04.1f'%(rah[i],ram[i],ras[i],decd[i],decm[i],decs[i])
+np.savetxt('/home/rumbaugh/MILLIQUAS_INY1A1TILE_WNAME.tab',crout,fmt='%6i %8i %9.5f %9.5f %21s %s',header='NUMROW MQ_ROWNUM RA DEC OBJNAME TILENAME')
