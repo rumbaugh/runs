b6d91b736774f12d249e3997933fdc690921c90c
Nick Rumbaugh <takkyon13@gmail.com>
2016-12-05 13:31:29 -0800

    vingilot - Mon Dec  5 13:31:29 PST 2016

--- run.12.3.16.2040.py
+++ run.12.3.16.2040.py
@@ -1,10 +1,15 @@
 import numpy as np
 import urllib
 
-url_template = 'http://skyserver.sdss.org/dr13/SkyServerWS/ImgCutout/getjpeg?ra={}&dec={}&scale=0.0853343447033446&width=600&height=600&opt=GI'
-filename_template = 'ra_{}_dec_{}.jpeg'
+url_template = 'http://skyserver.sdss.org/dr13/SkyServerWS/ImgCutout/getjpeg?ra={}&dec={}&scale=0.05&width=600&height=600&opt=GI'
+filename_template = '/home/rumbaugh/var_database/plots/SDSS_cutout.DBID_{}.jpeg'
 
-def save_image(ra, dec):
+def save_image(ra, dec,DBID):
     url = url_template.format(ra, dec)
-    filename = filename_template.format(ra, dec)
+    filename = filename_template.format(DBID)
     urllib.urlretrieve(url, filename=filename)
+
+cr=np.loadtxt('/home/rumbaugh/radecname_forSDSScutouts.csv',dtype={'names':('ra','dec','name'),'formats':('f8','f8','|S20')},delimiter=',')
+
+for ra,dec,DBID in zip(cr['ra'],cr['dec'],cr['DBID']):
+    save_image(ra,dec,DBID)
