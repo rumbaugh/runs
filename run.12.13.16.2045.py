b0db5259af097f1fd02dc02a53306edfd8f39436
Nick Rumbaugh <takkyon13@gmail.com>
2016-12-14 16:11:45 -0800

    vingilot - Wed Dec 14 16:11:45 PST 2016

--- run.12.13.16.2045.py
+++ run.12.13.16.2045.py
@@ -83,7 +83,7 @@ def plot_lightcurve(dbid,mjd,mag,magerr,bands,survey,trueredshift,plotSDSS=False
     plt.tick_params(which='major',length=8,width=2,labelsize=14)
     plt.tick_params(which='minor',length=4,width=1.5,labelsize=14)
     #plt.locator_params(nbins=4)
-    plt.plot(crv[:,0]/(1.+redshift),crv[:,1],color='k',lw=2)
+    ax.plot(crv[:,0]/(1.+redshift),crv[:,1],color='k',lw=2)
     gdes,gsdss,gposs,gndes=np.where(survey=='DES')[0],np.where(survey=='SDSS')[0],np.where(survey=='POSS')[0],np.where(survey!='DES')[0]
     if len(gposs)>0:
         POSSmagdict={b: mag[gposs][bands[gposs]==b] for b in POSSbands}
