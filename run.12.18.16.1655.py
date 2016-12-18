4d27b61ffa910db0b61193fe448162c013b81199
Nick Rumbaugh <takkyon13@gmail.com>
2016-12-18 14:56:44 -0800

    vingilot - Sun Dec 18 14:56:44 PST 2016

---
+++ run.12.18.16.1655.py
@@ -0,0 +1,92 @@
+import easyaccess as ea
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import matplotlib.backends.backend_pdf as bpdf
+
+crids=np.loadtxt('/home/rumbaugh/var_database/maxdiffs_DBID.12.18.16.txt',dtype={'names':('DBID','maxdiff'),'formats':('i8','f8')})
+
+good_dbids=crids['DBID'][crids['maxdiff']>2]
+
+DB_path='/home/rumbaugh/var_database'
+psfpdf=bpdf.PdfPages('/home/rumbaugh/var_database/plots/changinglookAGNcandidates_plots.12.18.16.pdf')
+coldict={'g': 'green','r': 'red', 'i': 'magenta', 'z': 'blue', 'Y': 'cyan'}
+SDSSbands=np.array(['u','g','r','i','z'])
+SDSS_colnames={b:'%s_SDSS'%b for b in SDSSbands}
+POSSbands=np.array(['g','r','i'])
+
+def plot_band(ax,mjd,mag,magerr,cbands,band,connectpoints=True,nolabels=False):
+    gband=np.where(cbands==band)[0]
+    magplot=mag[gband]
+    magploterr=magerr[gband]
+    g100=np.where(magplot<100)[0]
+    try:
+        curcol=coldict[band]
+    except KeyError:
+        print '%s is not a valid band'%band
+        return
+    if connectpoints:
+        gsort=np.argsort(mjd[gband][g100])
+        ax.plot(mjd[gband][g100][gsort],magplot[g100][gsort],color=curcol,lw=2)
+    ax.errorbar(mjd[gband][g100],magplot[g100],yerr=magploterr[g100],color=curcol,fmt='ro',lw=2,capsize=3,mew=1)
+    if nolabels:
+        ax.scatter(mjd[gband][g100],magplot[g100],color=curcol)
+    else:
+        ax.scatter(mjd[gband][g100],magplot[g100],color=curcol,label=band)
+    #return
+
+
+def plot_lightcurve(dbid,mjd,mag,magerr,bands,survey,plotSDSS=False,fname=None,connectpoints=True):
+    gdes,gsdss,gposs=np.where(survey=='DES')[0],np.where(survey=='SDSS')[0],np.where(survey=='POSS')[0]
+    if len(gposs)>0:
+        POSSmagdict={b: mag[gposs][bands[gposs]==b] for b in POSSbands}
+        for band in POSSbands:
+            if len(POSSmagdict[band])>0:POSSmagdict[band]=np.array([np.median(POSSmagdict[band])])
+        if (len(POSSmagdict['g'])>0)&(len(POSSmagdict['r'])>0):
+            mag[gposs][bands[gposs]=='g'],mag[gposs][bands[gposs]=='r']=mag[gposs][bands[gposs]=='g']+0.392*(POSSmagdict['g']-POSSmagdict['r'])-0.28, mag[gposs][bands[gposs]=='r'] +0.127*(POSSmagdict['g']-POSSmagdict['r'])+0.1
+        else: 
+            mag[gposs][bands[gposs]=='g'],mag[gposs][bands[gposs]=='r']=np.zeros(0),np.zeros(0)
+        if (len(POSSmagdict['i'])>0)&(len(POSSmagdict['r'])>0):   
+            mag[gposs][bands[gposs]=='i']=mag[gposs][bands[gposs]=='i']+0.27*(POSSmagdict['r']-POSSmagdict['i'])+0.32
+        else:
+            mag[gposs][bands[gposs]=='i']=np.zeros(0)
+    fig=plt.figure(1)
+    fig.clf()
+    ax=fig.add_subplot(2,1,2)
+    plt.rc('axes',linewidth=2)
+    plt.fontsize = 14
+    plt.tick_params(which='major',length=8,width=2,labelsize=14)
+    plt.tick_params(which='minor',length=4,width=1.5,labelsize=14)
+    for b in ['g','r','i','z','Y']:
+        plot_band(ax,mjd[gdes],mag[gdes],magerr[gdes],bands[gdes],b,connectpoints=connectpoints)
+    xlim=plt.xlim()
+    plt.xlim(xlim[0],xlim[1]+0.33*(xlim[1]-xlim[0]))
+    ylim=plt.ylim()
+    plt.ylim(ylim[1],ylim[0])
+    ax.legend()
+    ax=fig.add_subplot(2,1,1)
+    for b in ['g','r','i','z','Y']:
+        plot_band(ax,mjd,mag,magerr,bands,b,connectpoints=connectpoints,nolabels=True)
+    xlim=plt.xlim()
+    plt.xlim(xlim[0],xlim[1]+0.33*(xlim[1]-xlim[0]))
+    ylim=plt.ylim()
+    plt.ylim(ylim[1],ylim[0])
+    ax.legend()
+    ax.set_xlabel('MJD')
+    ax.set_ylabel('Mag_PSF')
+    ax.set_title(dbid)
+    plt.savefig(psfpdf,format='pdf')
+    return
+
+outputdir='/home/rumbaugh/var_database'
+
+#cr=np.loadtxt('SDSSPOSS_lightcurve_entries.tab',dtype={'names':('cid','SP_ROWNUM','ra_y1a1','dec_y1a1','sdr7id','mjd_SDSS','EPOCHG','EPOCHR','EPOCHI','ra','dec','G_POSS','R_POSS','I_POSS','G_POSS_err','R_POSS_err','I_POSS_err','G_SDSS','R_SDSS','I_SDSS','G_SDSS_err','R_SDSS_err','I_SDSS_err'),'formats':('i8','i8','f8','f8','i8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8')},skiprows=1)
+#cry=np.loadtxt('SDSSPOSS_lightcurve_entries_Y1A1.tab',skiprows=1,dtype={'names':('mjd','imageid','cid','SPid','ra','dec','mag','magerr','band','exp'),'formats':('f8','i8','i8','i8','f8','f8','f8','f8','|S12','f8')})
+
+for DBID in good_dbids:
+    cr=np.loadtxt('%s/%i/LC.tab'%(outputdir,DBID),dtype={'names':('DatabaseID','Survey','SurveyCoaddID','SurveyObjectID','RA','DEC','MJD','TAG','BAND','MAGTYPE','MAG','MAGERR','FLAG'),'formats':('i8','|S20','|S20','|S20','f8','f8','f8','|S20','|S12','|S12','f8','f8','i8')},skiprows=1)
+    curid=int(cr['SurveyCoaddID'][gdes[0]])
+    mjd,mag,magerr,bands,survey=cr['MJD'],cr['MAG'],cr['MAGERR'],cr['BAND'],cr['Survey']
+    plot_lightcurve(DBID,mjd,mag,magerr,bands,survey,plotSDSS=False)
+
+psfpdf.close()
