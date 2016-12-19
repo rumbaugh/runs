658af0d9bf821da94422e183189b841cf66b7b80
Nick Rumbaugh <takkyon13@gmail.com>
2016-12-19 15:52:20 -0800

    vingilot - Mon Dec 19 15:52:20 PST 2016

--- run.12.19.16.1745.py
+++ run.12.19.16.1745.py
@@ -3,6 +3,6 @@ import pyfits as py
 
 cr_rids=np.loadtxt('/home/rumbaugh/changinglookAGNcandidates_index.12.18.16.dat',dtype={'names':('DBID','CID','tid','sdr7id','ra','dec','IntFlag'),'formats':('i8','i8','i8','i8','f8','f8','i8')})
 
-crout=np.zeros((len(crids),2),dtype='i8')
+crout=np.zeros((len(cr_rids),2),dtype='i8')
 crout[:,0]=cr_rids['DBID']
 np.savetxt('/home/rumbaugh/changinglookAGNcandidates_bestindex.12.18.16.dat',crout,fmt='%12i %2i',header='DBID IntFlag')
