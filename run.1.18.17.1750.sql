9f137f5bac7c441ef29a6d3165349a0b62378bfb
Nick Rumbaugh <takkyon13@gmail.com>
2017-01-19 09:47:14 -0800

    vingilot - Thu Jan 19 09:47:14 PST 2017

--- run.1.18.17.1750.sql
+++ run.1.18.17.1750.sql
@@ -1,1 +1,1 @@
-SELECT h.coadd_object_id,y.ra,y.dec,h.hpix_16384 from DES_ADMIN.Y3A1_COADD_OBJECT_HPIX h, DES_ADMIN.Y3A1_COADD_OBJECT_SUMMARY y; >y3a1_hpix_radec.tab
+SELECT h.coadd_object_id,y.ra,y.dec,h.hpix_16384 from DES_ADMIN.Y3A1_COADD_OBJECT_HPIX h, DES_ADMIN.Y3A1_COADD_OBJECT_SUMMARY y where h.coadd_object_id=y.coadd_object_id; >y3a1_hpix_radec.tab
