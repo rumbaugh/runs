241a625ffcd7562d11206f07f3c72992db45b685
Nick Rumbaugh <takkyon13@gmail.com>
2017-01-30 14:40:28 -0800

    vingilot - Mon Jan 30 14:40:28 PST 2017

--- run.1.30.17.1545.py
+++ run.1.30.17.1545.py
@@ -9,7 +9,6 @@ except NameError:
     crmd=np.loadtxt('/home/rumbaugh/milliquas_y3a1_match_pass2.csv',dtype={'names':('MQ_ROWNUM','RA','DEC','HPIX','COADD_OBJECTS_ID'),'formats':('i8','f8','f8','i8','i8')},delimiter=',',skiprows=1)
 
 crm=crmd[crmd['COADD_OBJECTS_ID']!=0]
-crm=crm[:100]
 dists=np.zeros(len(crm))
 for i in range(0,len(crm)):
     cid,ra,dec=crm['COADD_OBJECTS_ID'][i],crm['RA'][i],crm['DEC'][i]
@@ -19,5 +18,5 @@ for i in range(0,len(crm)):
 
 plt.figure(1)
 plt.clf()
-plt.hist(dists)
+plt.hist(dists*3600)
 plt.savefig('/home/rumbaugh/MQ_match_test_y3a1.dist_hist.png')
