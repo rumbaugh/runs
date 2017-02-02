600a57d7967010f52654ddd2b27054fb2bef931c
Nick Rumbaugh <takkyon13@gmail.com>
2017-02-02 12:14:18 -0800

    vingilot - Thu Feb  2 12:14:17 PST 2017

--- run.2.1.17.1215.py
+++ run.2.1.17.1215.py
@@ -16,9 +16,10 @@ def read_spec_file(field):
     crspec=np.genfromtxt(fname,dtype=loaddict,usecols=uc)
     return crspec
 
-def zhist(zs,zrange=None,zbins=10,fname=None):
+def zhist(zs,zrange=None,zbins=10,fname=None,zclus=None):
     execfile('/home/rumbaugh/pythonscripts/set_plt_params.py')
     plt.hist(zs,range=zrange,bins=zbins)
+    if zclus!=None: plt.axvline(zclus,lw=2,ls='dashed',color='r')
     plt.xlabel('Redshift')
     plt.ylabel('Num. of galaxies')
     if fname!=None: plt.savefig(fname)
@@ -36,6 +37,9 @@ crf=np.loadtxt('/home/rumbaugh/Chandra/ORELSE.cluster_fits.dat',dtype={'names':(
 cr=np.loadtxt('/home/rumbaugh/Chandra/ORELSE.clusters.dat',dtype={'names':('field','cluster','RA','Dec','z','sig0.5','sig0.5err','n0.5','sig','sigerr','nsig'),'formats':('|S24','|S24','f8','f8','f8','f8','f8','f8','f8','f8','f8')})
 crx=np.loadtxt('/home/rumbaugh/Chandra/ORELSE.X-ray_centers_for_paper.dat',dtype={'names':('field','cluster','RA','DEC'),'formats':('|S20','|S20','f8','f8')},usecols=(0,1,2,3))
 
+crz=np.loadtxt('/home/rumbaugh/Chandra/ORELSE.clus_sigs_wZ.dat',dtype={'names':('field','cluster','sigall','saerr','Nsa','sigcut','scerr','Nsc','sigred','srerr','Nsr','sigblue','sberr','Nsb','Zlb','Zub'),'formats':('|S20','|S20','f8','f8','i8','f8','f8','i8','f8','f8','i8','f8','f8','i8','f8','f8')})
+
+
 cosmocalc(cr['z'],outfile='/home/rumbaugh/cc_out_clus.1.28.17.dat',ids=cr['cluster'])
 
 crcc=np.loadtxt('/home/rumbaugh/cc_out_clus.1.28.17.dat',usecols=(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19))
@@ -48,7 +52,7 @@ FILE.write('# Field Cluster Sig_all Error N Sig_all_cut Error N Sig_red Error N 
 for clus in crf['cluster']:
     try:
         gcr=np.where(cr['cluster']==clus)[0][0]
-        raC,decC=cr['RA'][gcr],cr['Dec'][gcr]
+        raC,decC,zC=cr['RA'][gcr],cr['Dec'][gcr],cr['z'][gcr]
         Mpc_am=1./Mpc[gcr]/60.
         field=cr['field'][gcr].lower()
     except:
@@ -58,15 +62,21 @@ for clus in crf['cluster']:
             raC,decC=crx['RA'][gcr],crx['DEC'][gcr]
             field=crx['field'][gcr].lower()
             zf=spec_dict[field]['z'][0]
-            cosmocalc(zf,outfile='/home/rumbaugh/temp_cc.1.31.17.dat')
-            crcctmp=np.loadtxt('/home/rumbaugh/temp_cc.1.31.17.dat')
+            cosmocalc(zf,outfile='/home/rumbaugh/temp_cc.2.1.17.dat')
+            crcctmp=np.loadtxt('/home/rumbaugh/temp_cc.2.1.17.dat')
             Mpc_am=1./(crcctmp[12]/1000.)/60.
+            zC=None
         except:
             print 'X-ray center not found either'
             continue
     gcCMD=np.where(crCMD['field']==field)[0]
     crspec=read_spec_file(field)
-    gspecz=np.where((crspec['Q']>2.5)&(crspec['z']>spec_dict[field]['z'][1])&(crspec['z']<spec_dict[field]['z'][2]))[0]
+    try:
+        gz=np.where(crz['cluster']==clus)[0][0]
+    except:
+        print "Didn't use this one before"
+        continue
+    gspecz=np.where((crspec['Q']>2.5)&(crspec['z']>crz['Zlb'][gz])&(crspec['z']<crz['Zub'][gz]))[0]
     tdists=SphDist(crspec['ra'][gspecz],crspec['dec'][gspecz],raC,decC)
     gclose=np.where(tdists<Mpc_am)[0]
     allzs=crspec['z'][gspecz][gclose]
@@ -77,15 +87,17 @@ for clus in crf['cluster']:
     print clus,allsig[0]
     plt.figure(1)
     plt.clf()
-    zhist(allzs[allsig[2]],fname='/home/rumbaugh/Chandra/plots/clus_sig.%s.%s.zhist.1.31.17.png'%(field,clus))
+    zhist(allzs[allsig[2]],zclus=zC,fname='/home/rumbaugh/Chandra/plots/clus_sig.%s.%s.zhist.wbounds.2.1.17.png'%(field,clus),zrange=(spec_dict[field]['z'][1],spec_dict[field]['z'][2]),zbins=int((-spec_dict[field]['z'][1]+spec_dict[field]['z'][2])/0.001))
     plt.figure(2)
     plt.clf()
-    spatplot(crspec['ra'][gspecz[gclose[allsig[2]]]],crspec['dec'][gspecz[gclose[allsig[2]]]],fname='/home/rumbaugh/Chandra/plots/clus_sig.%s.%s.spatplot.1.31.17.png'%(field,clus))
-    tdists=SphDist(crCMD['ra'][gcCMD],crCMD['dec'][gcCMD],raC,decC)
+    #spatplot(crspec['ra'][gspecz[gclose[allsig[2]]]],crspec['dec'][gspecz[gclose[allsig[2]]]],fname='/home/rumbaugh/Chandra/plots/clus_sig.%s.%s.spatplot.2.1.17.png'%(field,clus))
+    gz2=np.where((crCMD[gcCMD]['z']>spec_dict[field]['z'][1])&(crCMD[gcCMD]['z']<spec_dict[field]['z'][2]))[0]
+
+    tdists=SphDist(crCMD['ra'][gcCMD[gz2]],crCMD['dec'][gcCMD[gz2]],raC,decC)
     gclose=np.where(tdists<Mpc_am)[0]
-    allzs2=crCMD['z'][gcCMD][gclose]
+    allzs2=crCMD['z'][gcCMD[gz2[gclose]]]
     allsig2=CalcVelDisp(allzs2,returnclipped=True)
-    gblue,gred=np.where(crCMD['rso'][gcCMD][gclose]>1)[0],np.where(crCMD['rso'][gcCMD][gclose]<1)[0]
+    gblue,gred=np.where(crCMD['rso'][gcCMD[gz2[gclose]]]>1)[0],np.where(crCMD['rso'][gcCMD[gz2[gclose]]]<1)[0]
     if (len(gblue)>0)&(len(gred)>0):
         bluesig,redsig=CalcVelDisp(allzs2[gblue],returnclipped=True),CalcVelDisp(allzs2[gred],returnclipped=True)
     else:
