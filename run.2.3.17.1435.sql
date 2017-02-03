61e8f911a7150c3382160352da3903f541b7907d
Nick Rumbaugh <takkyon13@gmail.com>
2017-02-03 12:42:22 -0800

    vingilot - Fri Feb  3 12:42:22 PST 2017

---
+++ run.2.3.17.1435.sql
@@ -0,0 +1,1 @@
+SELECT e.mjd_obs,o.expnum,y.COADD_OBJECT_ID,o.RA,o.DEC,o.mag_pointsource,o.band,i.exptime FROM des_admin.Y3A1_COADD_OBJECT_SUMMARY y, des_admin.y3a1_finalcut_object o,des_admin.y3a1_exposure e,des_admin.y3a1_wavg_oclink l where o.expnum=e.expnum and y.coadd_object_id=l.coadd_objet_id and o.filename=l.se_object_filename and o.object_number=l.se_object_number;
