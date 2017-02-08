0300e634f84af53aa14f201fe293fd9cf9f0d2e0
Nick Rumbaugh <takkyon13@gmail.com>
2017-02-08 15:13:14 -0800

    vingilot - Wed Feb  8 15:13:14 PST 2017

--- run.2.8.17.1230.py
+++ run.2.8.17.1230.py
@@ -10,7 +10,7 @@ cr=np.loadtxt('/home/rumbaugh/Chandra/ORELSE.clus_sigs.dat',dtype={'names':('fie
 cr=cr[cr['field']!='cl0849']
 
 crv=np.loadtxt('/home/rumbaugh/Chandra/ORELSE.veldiffs.dat',dtype={'names':('field','cluster','blueVC','redVC','perc','percBS'),'formats':('|S12','|S20','f8','f8','f8','f8')})
-crv=cvr[crv['field']!='cl0849']
+crv=crv[crv['field']!='cl0849']
 veldiffs=(crv['redVC']-crv['blueVC'])*3.0*10.**5/(1.+crv['redVC'])
 
 Tarr=np.zeros((len(cr),9),dtype='object')
@@ -24,7 +24,7 @@ for i in range(0,len(cr)):
     Tarr[i][1]='%.0f+/-%.0f'%(cr['sigall'][i],cr['saerr'][i])
     Tarr[i][2]=cr['Nsa'][i]
     if ((cr['Nsb'][i]>=10)&(cr['Nsr'][i]>=10)):
-        Tarr[i][3:]=np.array(['%.0f+/-%.0f'%(cr['sigblue'][i],cr['sberr'][i]),cr['Nsb'][i],'%.0f+/-%.0f'%(cr['sigred'][i],cr['srerr'][i]),cr['Nsr'][i]],veldiffs[i],perc[i]*100)
+        Tarr[i][3:]=np.array(['%.0f+/-%.0f'%(cr['sigblue'][i],cr['sberr'][i]),cr['Nsb'][i],'%.0f+/-%.0f'%(cr['sigred'][i],cr['srerr'][i]),cr['Nsr'][i],veldiffs[i],crv['perc'][i]*100])
     else:
         Tarr[i][3:]='-'
     
@@ -32,5 +32,8 @@ for i in range(0,len(cr)):
 ax=fig.add_subplot(111)
 ax.xaxis.set_visible(False)
 ax.yaxis.set_visible(False)
-ax.table(cellText=Tarr,loc='center',colLabels=('Cluster','Full Disp.','N','Blue Disp.','N','Red Disp.','N','Vel. Diff.','P'))
-plt.savefig('/home/rumbaugh/Chandra/plots/veldisp_table.2.5.17.png')
+colwidrat=0.4
+wcol=1./(5.5+3*colwidrat)
+scol=colwidrat*wcol
+ax.table(cellText=Tarr,loc='center',colLabels=('Cluster','Full Disp.','N','Blue Disp.','N','Red Disp.','N','Vel. Diff.','P'),colWidths=(wcol,wcol,scol,wcol,scol,wcol,scol,wcol,wcol/2))
+plt.savefig('/home/rumbaugh/Chandra/plots/veldisp_table.2.8.17.png')
