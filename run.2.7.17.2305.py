96f98b70bc980a7b1a643a8c2d1dceaeadd501de
Nick Rumbaugh <takkyon13@gmail.com>
2017-02-08 17:25:00 -0800

    vingilot - Wed Feb  8 17:25:00 PST 2017

--- run.2.7.17.2305.py
+++ run.2.7.17.2305.py
@@ -6,7 +6,7 @@ crdb=np.loadtxt('/home/rumbaugh/var_database/Y3A1/database_index.dat',dtype={'na
 candidate_flag=np.zeros(len(crdb),dtype='bool')
 for DBID,idb in zip(crdb['DatabaseID'],np.arange(len(crdb))):
     cr=np.loadtxt('%s/Y3A1/%s/LC.tab'%(outputdir,DBID),dtype={'names':('DatabaseID','Survey','SurveyCoaddID','SurveyObjectID','RA','DEC','MJD','TAG','BAND','MAGTYPE','MAG','MAGERR','FLAG'),'formats':('|S64','|S20','|S20','|S20','f8','f8','f8','|S20','|S12','|S12','f8','f8','i8')},skiprows=1)
-    ggood=np.where((cr['MAG']>15)&(cr['MAG']<30)&(cr['FLAG']<16))[0]
+    ggood=np.where((cr['MAG']>15)&(cr['MAG']<30))[0]#&(cr['FLAG']<16))[0]
     cr=cr[ggood]
     mjd,mag,magerr,bands,survey=cr['MJD'],cr['MAG'],cr['MAGERR'],cr['BAND'],cr['Survey']
     for band in ['g','r','i','z']:
