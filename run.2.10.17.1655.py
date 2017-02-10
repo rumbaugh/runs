f33ef474d6838f740cf53cb46ccf8290e916f38a
Nick Rumbaugh <takkyon13@gmail.com>
2017-02-10 15:24:20 -0800

    vingilot - Fri Feb 10 15:24:19 PST 2017

--- run.2.10.17.1655.py
+++ run.2.10.17.1655.py
@@ -11,5 +11,6 @@ for b in bands:
     outcr_dict[b]['BAND']=b
     outcr_dict[b]['COADD_OBJECT_ID'],outcr_dict[b]['RA'],outcr_dict[b]['DEC'],outcr_dict[b]['FLAGS'] = np.repeat(data['COADD_OBJECT_ID'],np.shape(data['LC_MJD_%s'%b.upper()])[1]),np.repeat(data['RA'],np.shape(data['LC_MJD_%s'%b.upper()])[1]),np.repeat(data['DEC'],np.shape(data['LC_MJD_%s'%b.upper()])[1]),np.repeat(data['FLAGS_%s'%b.upper()],np.shape(data['LC_MJD_%s'%b.upper()])[1])
     outcr_dict[b]['MJD'],outcr_dict[b]['MAG_PSF'],outcr_dict[b]['MAG_PSF_ERROR'],outcr_dict[b]['MAG_AUTO'],outcr_dict[b]['MAG_AUTO_ERROR']=data['LC_MJD_%s'%b.upper()].flatten(),data['LC_MAG_PSF_%s'%b.upper()].flatten(),data['LC_MAGERR_PSF_%s'%b.upper()].flatten(),data['LC_MAG_AUTO_%s'%b.upper()].flatten(),data['LC_MAGERR_AUTO_%s'%b.upper()].flatten()
+    outcr_dict[b]=outcr_dict[b][outcr_dict[b]['LC_MJD_%s'%b.upper()]!=0]]
 outcr=np.concatenate((outcr_dict['g'],outcr_dict['r'],outcr_dict['i'],outcr_dict['z']),axis=0)
 np.savetxt('/home/rumbaugh/Eric_LC.Y3A1.dat',outcr,fmt='%i %f %f %f %f %f %f %f %4s %i',header='COADD_OBJECT_ID RA DEC MJD MAG_PSF MAG_PSF_ERROR MAG_AUTO MAG_AUTO_ERROR BAND FLAGS',comments='')
