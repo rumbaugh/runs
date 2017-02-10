b789bc308ac23cf2f02eecbbbef7943c7d9f1bbe
Nick Rumbaugh <takkyon13@gmail.com>
2017-02-09 17:38:28 -0800

    vingilot - Thu Feb  9 17:38:28 PST 2017

--- run.2.9.17.1915.py
+++ run.2.9.17.1915.py
@@ -5,7 +5,7 @@ crdb=np.loadtxt('/home/rumbaugh/var_database/Y3A1/database_index.dat',dtype={'na
 crdb=crdb[crdb['SDR7ID']==0]
 candidate_flag=np.zeros(len(crdb),dtype='bool')
 for DBID,idb in zip(crdb['DatabaseID'],np.arange(len(crdb))):
-    cr=np.loadtxt('%s/Y3A1/%s/LC.tab'%(outputdir,DBID),dtype={'names':('DatabaseID','Survey','SurveyCoaddID','SurveyObjectID','RA','DEC','MJD','TAG','BAND','MAGTYPE','MAG','MAGERR','FLAG'),'formats':('|S64','|S20','|S20','|S20','f8','f8','f8','|S20','|S12','|S12','f8','f8','i8')},skiprows=1)
+    cr=np.loadtxt('%s/Y3A1/%s/LC.tab'%(DB_path,DBID),dtype={'names':('DatabaseID','Survey','SurveyCoaddID','SurveyObjectID','RA','DEC','MJD','TAG','BAND','MAGTYPE','MAG','MAGERR','FLAG'),'formats':('|S64','|S20','|S20','|S20','f8','f8','f8','|S20','|S12','|S12','f8','f8','i8')},skiprows=1)
     ggood=np.where((cr['MAG']>15)&(cr['MAG']<30))[0]#&(cr['FLAG']<16))[0]
     cr=cr[ggood]
     mjd,mag,magerr,bands,survey=cr['MJD'],cr['MAG'],cr['MAGERR'],cr['BAND'],cr['Survey']
