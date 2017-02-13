e16a6dbfbcfa48e07dd4f246b6c791c3ae1731bf
Nick Rumbaugh <takkyon13@gmail.com>
2017-02-12 20:00:08 -0800

    vingilot - Sun Feb 12 20:00:08 PST 2017

--- run.2.10.17.1755.py
+++ run.2.10.17.1755.py
@@ -2,4 +2,4 @@ import numpy as np
 import pyfits as py
 
 cr=np.loadtxt('/home/rumbaugh/Eric_LC_Y3A1.tab',dtype={'names':('COADD_OBJECT_ID','RA','DEC','MJD','MAG_PSF','MAG_PSF_ERROR','MAG_AUTO','MAG_AUTO_ERROR','BAND','FLAGS'),'formats':('i8','f8','f8','f8','f8','f8','f8','f8','|S4','i8')},skiprows=1)
-py.PrimaryHDU(cr).writeto('/home/rumbaugh/Eric_LC_Y3A1.fits',cr,clobber=True)
+py.PrimaryHDU(cr).writeto('/home/rumbaugh/Eric_LC_Y3A1.fits',clobber=True)
