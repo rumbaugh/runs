7e85e6b702e94adaa3760608077cf9aa95764418
Nick Rumbaugh <takkyon13@gmail.com>
2017-02-13 18:49:25 -0800

    vingilot - Mon Feb 13 18:49:25 PST 2017

--- run.2.13.17.2040.py
+++ run.2.13.17.2040.py
@@ -2,6 +2,6 @@ import numpy as np
 cr=np.loadtxt('dr7_bh_y3a1_match.csv',dtype={'names':('SDSSNAME','ra','dec','hpix','cid'),'formats':('|S24','f8','f8','i8','i8')},skiprows=1,delimiter=',')
 #outcr=cr[cr['cid']!=0]
 #cr=cr[cr['cid']!=0]
-outcr=np.zeros(len(cr),dtype=[('SDSSNAME', '|S24'),('ra', '<f8'), ('dec', '<f8')])
-outcr['SDSSNAME'],outcr['ra'],outcr['dec']=cr['SDSSNAME'],cr['ra'],cr['dec']
-np.savetxt('/home/rumbaugh/dr7_bh_radec.csv',outcr,fmt='%s,%f,%f',header='SDSSNAME,RA,DEC',comments='')
+outcr=np.zeros(len(cr),dtype=[('numrow', '<i8'),('SDSSNAME', '|S24'), ('ra', '<f8'), ('dec', '<f8')])
+outcr['numrow'],outcr['SDSSNAME'],outcr['ra'],outcr['dec']=np.arange(len(outcr)),cr['SDSSNAME'],cr['ra'],cr['dec']
+np.savetxt('/home/rumbaugh/dr7_bh_radec.csv',outcr2,fmt='%i,%s,%f,%f',header='NUMROW,SDSSNAME,RA,DEC',comments='')
