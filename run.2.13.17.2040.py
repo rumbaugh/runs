432d063bdf55d99689f3a2e60c0bffdbc76a21dc
Nick Rumbaugh <takkyon13@gmail.com>
2017-02-13 18:49:41 -0800

    vingilot - Mon Feb 13 18:49:40 PST 2017

--- run.2.13.17.2040.py
+++ run.2.13.17.2040.py
@@ -4,4 +4,4 @@ cr=np.loadtxt('dr7_bh_y3a1_match.csv',dtype={'names':('SDSSNAME','ra','dec','hpi
 #cr=cr[cr['cid']!=0]
 outcr=np.zeros(len(cr),dtype=[('numrow', '<i8'),('SDSSNAME', '|S24'), ('ra', '<f8'), ('dec', '<f8')])
 outcr['numrow'],outcr['SDSSNAME'],outcr['ra'],outcr['dec']=np.arange(len(outcr)),cr['SDSSNAME'],cr['ra'],cr['dec']
-np.savetxt('/home/rumbaugh/dr7_bh_radec.csv',outcr2,fmt='%i,%s,%f,%f',header='NUMROW,SDSSNAME,RA,DEC',comments='')
+np.savetxt('/home/rumbaugh/dr7_bh_radec.csv',outcr,fmt='%i,%s,%f,%f',header='NUMROW,SDSSNAME,RA,DEC',comments='')
