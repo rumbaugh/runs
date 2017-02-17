7b73023a88cf7974196d51bd5f0c9236690d8ffd
Nick Rumbaugh <takkyon13@gmail.com>
2017-02-17 07:37:30 -0800

    vingilot - Fri Feb 17 07:37:30 PST 2017

---
+++ run.2.17.17.0935.py
@@ -0,0 +1,15 @@
+import numpy as np
+import urllib
+
+url_template = 'http://skyserver.sdss.org/dr13/SkyServerWS/ImgCutout/getjpeg?ra={}&dec={}&scale=0.004&width=600&height=600&opt=GI'
+filename_template = '/home/rumbaugh/var_database/Y3A1/imagestamps/{}.jpeg'
+
+def save_image(ra, dec,DBID):
+    url = url_template.format(ra, dec)
+    filename = filename_template.format(DBID)
+    urllib.urlretrieve(url, filename=filename)
+
+cr=np.loadtxt('/home/rumbaugh/radecname_forSDSScutouts.2.16.17.csv',dtype={'names':('ra','dec','name'),'formats':('f8','f8','|S40')},delimiter=',')
+
+for ra,dec,DBID in zip(cr['ra'],cr['dec'],cr['name']):
+    save_image(ra,dec,DBID)
