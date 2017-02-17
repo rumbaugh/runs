2d5fbb4eccff8513a521d1bb9f3050c90beb42a1
Nick Rumbaugh <takkyon13@gmail.com>
2017-02-17 15:50:32 -0800

    vingilot - Fri Feb 17 15:50:32 PST 2017

--- run.2.17.17.1750.py
+++ run.2.17.17.1750.py
@@ -1,5 +1,5 @@
 import numpy as np
-cr=np.loadtxt('dr7_bh_y3a1_match.csv',dtype={'names':('SDSSNAME','ra','dec','hpix','cid'),'formats':('|S24','f8','f8','i8','i8')},skiprows=1,delimiter=',')
+cr=np.loadtxt('/home/rumbaugh/dr7_bh_y3a1_match.csv',dtype={'names':('SDSSNAME','ra','dec','hpix','cid'),'formats':('|S24','f8','f8','i8','i8')},skiprows=1,delimiter=',')
 #outcr=cr[cr['cid']!=0]
 #cr=cr[cr['cid']!=0]
 outcr=np.zeros(len(cr),dtype=[('numrow', '<i8'),('SDSSNAME', '|S24'), ('ra', '<f8'), ('dec', '<f8')])
