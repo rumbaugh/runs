5a308d565420a7b027ab3dd6f12b3b68080bc615
Nick Rumbaugh <takkyon13@gmail.com>
2017-02-17 08:30:55 -0800

    vingilot - Fri Feb 17 08:30:55 PST 2017

---
+++ run.2.17.17.1030.py
@@ -0,0 +1,3 @@
+execfile('/home/rumbaugh/pythonscripts/plot_DB_lightcurves.py')
+
+plot_CLQ_candidates(2,'/home/rumbaugh/var_database/plots/CLQ_candidates.2.16.17.pdf',DBdir='/home/rumbaugh/var_database/Y3A1')
