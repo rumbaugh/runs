f63b9dceaebeeb211050013035d1399ab0fff189
Nick Rumbaugh <takkyon13@gmail.com>
2017-02-18 22:27:16 -0800

    vingilot - Sat Feb 18 22:27:16 PST 2017

--- run.2.19.17.0010.py
+++ run.2.19.17.0010.py
@@ -7,7 +7,7 @@ cr=np.loadtxt('/home/rumbaugh/var_database/Y3A1/databaseIDs.dat',dtype={'names':
 gdb=np.where((crdb['MQrownum']!=-1)&(crdb['SDSSNAME']!='-1'))[0]
 gdb=gdb[:4]
 
-tarlist=np.zeros(len(gdb),dtype='str')
+tarlist=np.zeros(len(gdb),dtype='|S64')
 for DBID,idb in zip(cr['DatabaseID'][gdb],np.arange(len(gdb))):
     os.system('cp -r %s/%s %s/../bkup_2.19.17/.'%(DB_path,DBID,DB_path))
     cr=np.loadtxt('%s/%s/LC.tab'%(outputdir,DBID),dtype={'names':('DatabaseID','Survey','SurveyCoaddID','SurveyObjectID','RA','DEC','MJD','TAG','BAND','MAGTYPE','MAG','MAGERR','FLAG'),'formats':('|S64','|S20','|S20','|S20','f8','f8','f8','|S20','|S12','|S12','f8','f8','i8')},skiprows=1)
