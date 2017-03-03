c2de1c89472a4afd3a29e148fe2e8ba60a919fe6
Nick Rumbaugh <takkyon13@gmail.com>
2017-03-02 21:22:33 -0800

    vingilot - Thu Mar  2 21:22:33 PST 2017

---
+++ run.3.2.17.2210.py
@@ -0,0 +1,45 @@
+execfile('/home/rumbaugh/pythonscripts/plot_DB_lightcurves.py')
+
+crmd=np.loadtxt('/home/rumbaugh/DR7_CLQ_candidate_flags.dat',dtype={'names':('DBID','drop','flag'),'formats':('|S32','f8','i8')},skiprows=1)
+
+good_dbids=crmd['DBID'][crmd['flag']>0]
+extra_good_dbids=crmd['DBID'][crmd['flag']>1]
+
+hdubh=py.open('/home/rumbaugh/dr7_bh_Nov19_2013.fits')
+bhdata=hdubh[1].data
+gl=np.where(bhdata['LOGLBOL']>0)[0]
+bhdata=bhdata[gl]
+bhz,bhname,bhL=bhdata['REDSHIFT'],bhdata['SDSS_NAME'],bhdata['LOGLBOL']
+
+ggd=np.zeros(len(good_dbids),dtype='i8')
+for i in range(0,len(ggd)): ggd[i]=np.where(bhname==good_dbids[i][5:])[0][0]
+gegd=np.zeros(len(extra_good_dbids),dtype='i8')
+for i in range(0,len(gegd)): gegd[i]=np.where(bhname==extra_good_dbids[i][5:])[0][0]
+
+
+execfile('/home/rumbaugh/pythonscripts/set_plt_params.py')
+
+plt.figure(1)
+plt.clf()
+Lmin,Lmax,zmin,zmax=np.min(bhL),np.max(bhL),np.min(bhz),np.max(bhz)
+tsize=30
+Lbnds,zbnds=np.linspace(Lmin,Lmax,tsize+1),np.linspace(zmin,zmax,tsize+1)
+zcens,Lcens=0.5*(zbnds[:-1]+zbnds[1:]),0.5*(Lbnds[:-1]+Lbnds[1:])
+zsize,Lsize=zcens[1]-zcens[0],Lcens[1]-Lcens[0]
+#zL_pairs=np.zeros((2,tsize**2))
+#zL_pairs[0],zL_pairs[1]=np.repeat(zcens,len(Lcens)),np.tile(Lcens,len(zcens))
+#richness=np.zeros(len(zL_pairs[0]))
+zL_pairs=np.meshgrid(zcens,Lcens)
+richness=np.zeros(np.shape(zL_pairs[0]))
+for i in np.arange(tsize):
+    for j in np.arange(tsize):
+        cur_bhz,cur_bhL=zL_pairs[0][i][j],zL_pairs[1][i][j]
+        richness[i][j]=len(np.where((np.abs(cur_bhz-bhz)<=0.5*zsize)&(np.abs(cur_bhL-bhL)<=0.5*Lsize))[0])
+plt.contour(zL_pairs[0],zL_pairs[1],richness)
+plt.scatter(bhz[ggd],bhL[ggd],color='r')
+plt.scatter(bhz[gegd],bhL[gegd],color='magenta',marker='x')
+plt.xlabel('Redshift')
+plt.ylabel(r'$log\left(L_{BOL}\right)')
+plt.xlim(zmin,zmax)
+plt.ylim(Lmin,Lmax)
+plt.savefig('/home/rumbaugh/var_database/Y3A1/plots/L-z_plot.DR7_CLQ_candidates.3.2.17.png')
