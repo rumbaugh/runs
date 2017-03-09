e5979abcf6c2ea351be5002d1bf6725f3bf0c3db
Nick Rumbaugh <takkyon13@gmail.com>
2017-03-08 21:40:15 -0800

    vingilot - Wed Mar  8 21:40:15 PST 2017

---
+++ run.3.8.17.2340.sh
@@ -0,0 +1,3 @@
+sed -i -e 's/"/ /g' /home/rumbaugh/milliquas_lightcurve_entries_y3a1_SN_abridged.tab
+sed -i -e 's/"/ /g' /home/rumbaugh/sdssposs_lightcurve_entries_y3a1_SN_abridged.tab
+sed -i -e 's/"/ /g' /home/rumbaugh/dr7_bh_lightcurve_entries_y3a1_SN.tab
---
+++ run.3.8.17.2340.sh~
@@ -0,0 +1,2 @@
+sed -i -e 's/"/ /g' /home/rumbaugh/dr7_bh_lightcurve_entries_SDSS.y3a1.tab
+sed -i -e 's/"/ /g' /home/rumbaugh/dr7_bh_lightcurve_entries_y3a1.tab
