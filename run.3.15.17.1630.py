a0a6d9a146b780701f199531212771413fef9a9c
Nick Rumbaugh <takkyon13@gmail.com>
2017-03-15 14:34:19 -0700

    vingilot - Wed Mar 15 14:34:19 PDT 2017

--- run.3.15.17.1630.py
+++ run.3.15.17.1630.py
@@ -1,12 +1,9 @@
 import numpy as np
-try:
-    ntrials
-except NameError:
-    ntrials=10000
 
-ra0,dec0=33.2532,0.21414
+def coordtest(ntrials,d=0.1/3600,ra0=33.2532,dec0=0.21414):
 
-ras,decs=np.random.normal(ra0,0.1/3600,2*ntrials),np.random.normal(dec0,0.1/3600,2*ntrials)
+    ras,decs=np.random.normal(ra0,0.1/3600,2*ntrials),np.random.normal(dec0,0.1/3600,2*ntrials)
 
-dists=SphDist(ras[:ntrials],decs[:ntrials],ras[ntrials:],decs[ntrials:])
+    dists=SphDist(ras[:ntrials],decs[:ntrials],ras[ntrials:],decs[ntrials:])
 
+    return 60*dists
