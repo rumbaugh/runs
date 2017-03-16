926e556ec158f739bea3d05c0025965d130112a9
Nick Rumbaugh <takkyon13@gmail.com>
2017-03-16 15:35:47 -0700

    vingilot - Thu Mar 16 15:35:46 PDT 2017

---
+++ run.3.16.17.1730.py
@@ -0,0 +1,13 @@
+import numpy as np
+execfile('/home/rumbaugh/pythonscripts/set_plot_params.py')
+
+cr=np.loadtxt('/home/rumbaugh/var_database/Y3A1/CLQ_candidates_DR7.3.8.17.dat',dtype={'names':('DBID','drop','S1','S2','S82','flag'),'formats':('|S24','f8','|S4','|S4','i8','i8')},skiprows=1)
+
+cr=cr[cr['flag']==0]
+
+plt.figure(1)
+plt.clf()
+plt.hist(np.abs(cr['drop']),range=(0,3.25),bins=13)
+plt.xlabel('Magnitude Change')
+plt.ylabel('Number of objects')
+plt.savefig('/home/rumbaugh/var_database/Y3A1/plots/MagDropPlot.CLQ_candidates.DR7.3.16.17.png')
