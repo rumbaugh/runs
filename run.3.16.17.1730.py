b09e17cb3617da2380c11084efcbb3ef62d9439e
Nick Rumbaugh <takkyon13@gmail.com>
2017-03-16 15:36:22 -0700

    vingilot - Thu Mar 16 15:36:22 PDT 2017

--- run.3.16.17.1730.py
+++ run.3.16.17.1730.py
@@ -1,5 +1,5 @@
 import numpy as np
-execfile('/home/rumbaugh/pythonscripts/set_plot_params.py')
+execfile('/home/rumbaugh/pythonscripts/set_plt_params.py')
 
 cr=np.loadtxt('/home/rumbaugh/var_database/Y3A1/CLQ_candidates_DR7.3.8.17.dat',dtype={'names':('DBID','drop','S1','S2','S82','flag'),'formats':('|S24','f8','|S4','|S4','i8','i8')},skiprows=1)
 
