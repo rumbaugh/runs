dd8a50cc4177ff6922460fd5195ab2fac0b4cc4c
Nick Rumbaugh <takkyon13@gmail.com>
2017-03-22 14:07:13 -0700

    vingilot - Wed Mar 22 14:07:13 PDT 2017

--- run.3.22.17.1600.py
+++ run.3.22.17.1600.py
@@ -9,6 +9,10 @@ crmc=np.genfromtxt('/home/rumbaugh/macleodQSOs/DB_QSO_S82.dat',dtype=mcdict,deli
 
 crdb=np.loadtxt('/home/rumbaugh/var_database/Y3A1/databaseIDs.dat',dtype={'names':('DatabaseID','DBIDS','MQrownum','SP_rownum','sdr7id','thingid','SDSSNAME','CID','TILENAME'),'formats':('|S32','|S128','i8','i8','|S24','i8','|S64','i8','|S32')},skiprows=1)
 
+hdu=py.open('/home/rumbaugh/var_database/Y3A1/masterfile.fits')
+data=hdu[1].data
+
+data=data[crdb['SDSSNAME']!='-1']
 crdb=crdb[crdb['SDSSNAME']!='-1']
 
-
+gdb,gmac,dists=pydl.pydutils.spheregroup.spherematch(crdb['
