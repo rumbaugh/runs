315ec5124c801a8be4eba53bfe47aafa05d1e78d
Nick Rumbaugh <takkyon13@gmail.com>
2017-03-22 14:07:48 -0700

    vingilot - Wed Mar 22 14:07:48 PDT 2017

--- run.3.22.17.1600.py
+++ run.3.22.17.1600.py
@@ -3,7 +3,7 @@ import healpy as hp
 import pydl
 import pydl.pydlutils
 
-mcdict={'names':('DBID','RA','DEC','SDR5ID','Mi','Micorr','redshift','massBH','Lbol','u','g','r','i','z','Au'),'formats':('i8','f8','f8','i8','f8','f8','f8',,'f8','f8','f8',,'f8','f8','f8',,'f8','f8')}
+mcdict={'names':('DBID','RA','DEC','SDR5ID','Mi','Micorr','redshift','massBH','Lbol','u','g','r','i','z','Au'),'formats':('i8','f8','f8','i8','f8','f8','f8',,'f8','f8','f8','f8','f8','f8',,'f8','f8')}
 delims=(8,11,11,6,8,8,7,6,7,7,7,7,7,7,7)
 crmc=np.genfromtxt('/home/rumbaugh/macleodQSOs/DB_QSO_S82.dat',dtype=mcdict,delimiter=delims)
 
