9407ae62c00c20255e7b69d49120d5c03abd3809
Nick Rumbaugh <takkyon13@gmail.com>
2017-03-22 14:49:51 -0700

    vingilot - Wed Mar 22 14:49:51 PDT 2017

--- run.3.22.17.1600.py
+++ run.3.22.17.1600.py
@@ -15,4 +15,21 @@ data=hdu[1].data
 data=data[crdb['SDSSNAME']!='-1']
 crdb=crdb[crdb['SDSSNAME']!='-1']
 
-#gdb,gmac,dists=pydl.pydutils.spheregroup.spherematch(crdb['
+gdb,gmac,dists=pydl.pydutils.spheregroup.spherematch(data['RA_SDSS'],data['Dec_SDSS'],crmc['RA'],crmc['DEC'],0.2/3600)
+
+outcr=np.zeros((len(data),),dtype={'names':('DBID','MCID'),'formats':('|S24','i8')})
+outcr['DBID'],outcr['MCID']=data['DatabaseID'],-1
+for i in range(0,len(outcr)):
+    DBID=outcr['DBID'][i]
+    g=np.where(DBID==outcr['DBID'][gdb])[0]
+    if len(g)>0:
+        outcr['MCID'][i]=crmc['DBID'][gmac[g[0]]]
+        mcLCdict={'names':('MJD_u','mag_u','mag_u_err','MJD_u','mag_u','mag_u_err','MJD_u','mag_u','mag_u_err','MJD_u','mag_u','mag_u_err','MJD_u','mag_u','mag_u_err','ra','dec'),'formats':('f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8')}
+        crlc=np.loadtxt('/home/rumbaugh/QSO_S82/%i'%(crmc['DBID'][gmac[g[0]]]),dtype=mcLCdict)
+        LCcr=np.zeros((len(crlc)*5,),dtype={'names':('DatabaseID','RA','DEC','MJD','BAND','MAG','MAGERR','FLAG'),'formats':('i8','f8','f8','f8','|S4','f8','f8','i8')})
+        LCcr['DatabaseID']=outcr['MCID'][i]
+        for b,ib in zip(['u','g','r','i','z'],np.arange(5)):
+            LCcr['MJD'][ib*len(crlc):(ib+1)*len(crlc)],LCcr['BAND'][ib*len(crlc):(ib+1)*len(crlc)],LCcr['MAG'][ib*len(crlc):(ib+1)*len(crlc)],LCcr['MAGERR'][ib*len(crlc):(ib+1)*len(crlc)],LCcr['RA'][ib*len(crlc):(ib+1)*len(crlc)],LCcr['DEC'][ib*len(crlc):(ib+1)*len(crlc)]=crlc['MJD_%s'%b],crlc['mag_%s'%b],crlc['mag_%s_err'%b],crlc['ra'],crlc['dec']
+        np.savetxt('/home/rumbaugh/var_database/Y3A1/%s/Macleod_LC.tab'%DBID,
+np.savetxt('/home/rumbaugh/var_database/Y3A1/DR7_Macleod_S82_match.dat',outcr,fmt='%24s %i',header='DatabaseID Macleod_DatabaseID',comments='')
+
