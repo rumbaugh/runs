b4978144bcae33959708961f4c717b2f12e6cc21
Nick Rumbaugh <takkyon13@gmail.com>
2017-03-24 09:26:13 -0700

    vingilot - Fri Mar 24 09:26:13 PDT 2017

--- run.3.24.17.1040.py
+++ run.3.24.17.1040.py
@@ -67,10 +67,17 @@ execfile('/home/rumbaugh/pythonscripts/set_plt_params.py')
 
 plt.figure(1)
 plt.clf()
-plt.plot(np.sort(cr['drop']),(np.arange(len(cr))+1.)/len(cr),lw=2,color='k')
+plt.plot(np.sort(np.abs(cr['drop'])),(np.arange(len(cr))+1.)/len(cr),lw=2,color='k')
 plt.xlabel('Magnitude Change')
 plt.ylabel('Cumulative Fraction')
 plt.savefig('/home/rumbaugh/var_database/Y3A1/plots/MagDropPlot.CLQ_candidates.DR7.3.24.17.png')
+
+plt.figure(1)
+plt.clf()
+plt.plot(np.sort(cr['Baseline']),(np.arange(len(cr))+1.)/len(cr),lw=2,color='k')
+plt.xlabel('Maximum Change Baseline (days)')
+plt.ylabel('Cumulative Fraction')
+plt.savefig('/home/rumbaugh/var_database/Y3A1/plots/MaxChangeBaselinePlot.CLQ_candidates.DR7.3.24.17.png')
 
 plt.figure(1)
 plt.clf()
@@ -88,23 +95,20 @@ execfile('/home/rumbaugh/pythonscripts/set_plt_params.py')
 
 plt.figure(1)
 plt.clf()
-Lmin,Lmax,zmin,zmax=np.min(bhL),np.max(bhL),np.min(bhz),np.max(bhz)
-tsize=30
-Lbnds,zbnds=np.linspace(Lmin,Lmax,tsize+1),np.linspace(zmin,zmax,tsize+1)
-zcens,Lcens=0.5*(zbnds[:-1]+zbnds[1:]),0.5*(Lbnds[:-1]+Lbnds[1:])
-zsize,Lsize=zcens[1]-zcens[0],Lcens[1]-Lcens[0]
-#zL_pairs=np.zeros((2,tsize**2))
-#zL_pairs[0],zL_pairs[1]=np.repeat(zcens,len(Lcens)),np.tile(Lcens,len(zcens))
-#richness=np.zeros(len(zL_pairs[0]))
-zL_pairs=np.meshgrid(zcens,Lcens)
-richness=np.zeros(np.shape(zL_pairs[0]))
-for i in np.arange(tsize):
-    for j in np.arange(tsize):
-        cur_bhz,cur_bhL=zL_pairs[0][i][j],zL_pairs[1][i][j]
-        richness[i][j]=len(np.where((np.abs(cur_bhz-bhz)<=0.5*zsize)&(np.abs(cur_bhL-bhL)<=0.5*Lsize))[0])
+#Lmin,Lmax,zmin,zmax=np.min(bhL),np.max(bhL),np.min(bhz),np.max(bhz)
+#tsize=30
+#Lbnds,zbnds=np.linspace(Lmin,Lmax,tsize+1),np.linspace(zmin,zmax,tsize+1)
+#zcens,Lcens=0.5*(zbnds[:-1]+zbnds[1:]),0.5*(Lbnds[:-1]+Lbnds[1:])
+#zsize,Lsize=zcens[1]-zcens[0],Lcens[1]-Lcens[0]
+#zL_pairs=np.meshgrid(zcens,Lcens)
+#richness=np.zeros(np.shape(zL_pairs[0]))
+#for i in np.arange(tsize):
+#    for j in np.arange(tsize):
+#        cur_bhz,cur_bhL=zL_pairs[0][i][j],zL_pairs[1][i][j]
+#        richness[i][j]=len(np.where((np.abs(cur_bhz-bhz)<=0.5*zsize)&(np.abs(cur_bhL-bhL)<=0.5*Lsize))[0])
 plt.scatter(bhz,bhL,s=2,edgecolor='None',marker='.',color='k')
 plt.scatter(cz,cL,s=8,edgecolor='None',marker='.',color='b')
-plt.contour(zL_pairs[0],zL_pairs[1],richness,color='k')
+#plt.contour(zL_pairs[0],zL_pairs[1],richness,color='k')
 plt.scatter(bhz[ggd],bhL[ggd],color='green',s=clqsize)
 plt.scatter(bhz[gegd],bhL[gegd],color='magenta',s=clqsize+2)
 plt.scatter(bhz[geegd],bhL[geegd],color='red',s=clqsize+4)
@@ -128,22 +132,22 @@ bhOIII,bhHB,bhFe=bhOIII[ggs],bhHB[ggs],bhFe[ggs]
 plt.figure(1)
 plt.clf()
 #HBmin,HBmax,Femin,Femax=np.min(bhHB),np.max(bhHB),np.min(bhFe),np.max(bhFe)
-HBmin,HBmax,Femin,Femax=0,10000,0,200
-tsize=30
-HBbnds,Febnds=np.linspace(HBmin,HBmax,tsize+1),np.linspace(Femin,Femax,tsize+1)
-Fecens,HBcens=0.5*(Febnds[:-1]+Febnds[1:]),0.5*(HBbnds[:-1]+HBbnds[1:])
-Fesize,HBsize=Fecens[1]-Fecens[0],HBcens[1]-HBcens[0]
+#HBmin,HBmax,Femin,Femax=0,10000,0,200
+#tsize=30
+#HBbnds,Febnds=np.linspace(HBmin,HBmax,tsize+1),np.linspace(Femin,Femax,tsize+1)
+#Fecens,HBcens=0.5*(Febnds[:-1]+Febnds[1:]),0.5*(HBbnds[:-1]+HBbnds[1:])
+#Fesize,HBsize=Fecens[1]-Fecens[0],HBcens[1]-HBcens[0]
 #zHB_pairs=np.zeros((2,tsize**2))
 #zHB_pairs[0],zHB_pairs[1]=np.repeat(zcens,len(HBcens)),np.tile(HBcens,len(zcens))
 #richness=np.zeros(len(zHB_pairs[0]))
-FeHB_pairs=np.meshgrid(Fecens,HBcens)
-richness=np.zeros(np.shape(FeHB_pairs[0]))
-for i in np.arange(tsize):
-    for j in np.arange(tsize):
-        cur_bhFe,cur_bhHB=FeHB_pairs[0][i][j],FeHB_pairs[1][i][j]
-        richness[i][j]=len(np.where((np.abs(cur_bhFe-bhFe)<=0.5*Fesize)&(np.abs(cur_bhHB-bhHB)<=0.5*HBsize))[0])
+#FeHB_pairs=np.meshgrid(Fecens,HBcens)
+#richness=np.zeros(np.shape(FeHB_pairs[0]))
+#for i in np.arange(tsize):
+#    for j in np.arange(tsize):
+#        cur_bhFe,cur_bhHB=FeHB_pairs[0][i][j],FeHB_pairs[1][i][j]
+#        richness[i][j]=len(np.where((np.abs(cur_bhFe-bhFe)<=0.5*Fesize)&(np.abs(cur_bhHB-bhHB)<=0.5*HBsize))[0])
 plt.scatter(bhFe,bhHB,s=2,edgecolor='None',marker='.',color='k')
-plt.contour(FeHB_pairs[0],FeHB_pairs[1],richness,color='k')
+#plt.contour(FeHB_pairs[0],FeHB_pairs[1],richness,color='k')
 ggz,gegz,geegz=np.where((bhFeorig[ggd]>0)&(bhHBorig[ggd]>0))[0],np.where((bhFeorig[gegd]>0)&(bhHBorig[gegd]>0))[0],np.where((bhFeorig[geegd]>0)&(bhHBorig[geegd]>0))[0]
 plt.scatter(bhFeorig[ggd[ggz]],bhHBorig[ggd[ggz]],color='green',s=clqsize)
 plt.scatter(bhFeorig[gegd[gegz]],bhHBorig[gegd[gegz]],color='magenta',s=clqsize+2)
@@ -161,21 +165,18 @@ plt.figure(1)
 plt.clf()
 bhri,bhz=medr_all-medi_all,medu_all-medg_all
 plt.scatter(medr_all-medi_all,medu_all-medg_all,color='k',s=2,edgecolor='None',marker='.')
-rimin,rimax,zmin,zmax=np.min(medr_all-medi_all),np.max(medr_all-medi_all),np.min(medu_all-medg_all),np.max(medu_all-medg_all)
-tsize=30
-ribnds,zbnds=np.linspace(rimin,rimax,tsize+1),np.linspace(zmin,zmax,tsize+1)
-zcens,ricens=0.5*(zbnds[:-1]+zbnds[1:]),0.5*(ribnds[:-1]+ribnds[1:])
-zsize,risize=zcens[1]-zcens[0],ricens[1]-ricens[0]
-#zL_pairs=np.zeros((2,tsize**2))
-#zL_pairs[0],zL_pairs[1]=np.repeat(zcens,len(Lcens)),np.tile(Lcens,len(zcens))
-#richness=np.zeros(len(zL_pairs[0]))
-zri_pairs=np.meshgrid(zcens,ricens)
-richness=np.zeros(np.shape(zri_pairs[0]))
-for i in np.arange(tsize):
-    for j in np.arange(tsize):
-        cur_bhz,cur_bhri=zri_pairs[0][i][j],zri_pairs[1][i][j]
-        richness[i][j]=len(np.where((np.abs(cur_bhz-bhz)<=0.5*zsize)&(np.abs(cur_bhri-bhri)<=0.5*risize))[0])
-plt.contour(zri_pairs[0],zri_pairs[1],richness,color='k')
+#rimin,rimax,zmin,zmax=np.min(medr_all-medi_all),np.max(medr_all-medi_all),np.min(medu_all-medg_all),np.max(medu_all-medg_all)
+#tsize=30
+#ribnds,zbnds=np.linspace(rimin,rimax,tsize+1),np.linspace(zmin,zmax,tsize+1)
+#zcens,ricens=0.5*(zbnds[:-1]+zbnds[1:]),0.5*(ribnds[:-1]+ribnds[1:])
+#zsize,risize=zcens[1]-zcens[0],ricens[1]-ricens[0]
+#zri_pairs=np.meshgrid(zcens,ricens)
+#richness=np.zeros(np.shape(zri_pairs[0]))
+#for i in np.arange(tsize):
+#    for j in np.arange(tsize):
+#        cur_bhz,cur_bhri=zri_pairs[0][i][j],zri_pairs[1][i][j]
+#        richness[i][j]=len(np.where((np.abs(cur_bhz-bhz)<=0.5*zsize)&(np.abs(cur_bhri-bhri)<=0.5*risize))[0])
+#plt.contour(zri_pairs[0],zri_pairs[1],richness,color='k')
 plt.scatter(medr[np.abs(cr['drop'])>1]-medi[np.abs(cr['drop'])>1],medu[np.abs(cr['drop'])>1]-medg[np.abs(cr['drop'])>1],color='green',s=clqsize/2)
 plt.scatter(medr[(np.abs(cr['drop'])>1)&(np.abs(cr['drop'])>1.5)]-medi[(np.abs(cr['drop'])>1)&(np.abs(cr['drop'])>1.5)],medu[(np.abs(cr['drop'])>1)&(np.abs(cr['drop'])>1.5)]-medg[(np.abs(cr['drop'])>1)&(np.abs(cr['drop'])>1.5)],color='magenta',s=clqsize/2+2)
 plt.scatter(medr[(np.abs(cr['drop'])>1)&(np.abs(cr['drop'])>2)]-medi[(np.abs(cr['drop'])>1)&(np.abs(cr['drop'])>2)],medu[(np.abs(cr['drop'])>1)&(np.abs(cr['drop'])>2)]-medg[(np.abs(cr['drop'])>1)&(np.abs(cr['drop'])>2)],color='red',s=clqsize/2+4)
@@ -190,21 +191,18 @@ plt.figure(1)
 plt.clf()
 bhri,bhz=medr_all-medi_all,medu_all-medg_all
 plt.scatter(medg_all-medr_all,medu_all-medg_all,color='k',s=2,edgecolor='None',marker='.')
-rimin,rimax,zmin,zmax=np.min(medg_all-medr_all),np.max(medg_all-medr_all),np.min(medu_all-medg_all),np.max(medu_all-medg_all)
-tsize=30
-ribnds,zbnds=np.linspace(rimin,rimax,tsize+1),np.linspace(zmin,zmax,tsize+1)
-zcens,ricens=0.5*(zbnds[:-1]+zbnds[1:]),0.5*(ribnds[:-1]+ribnds[1:])
-zsize,risize=zcens[1]-zcens[0],ricens[1]-ricens[0]
-#zL_pairs=np.zeros((2,tsize**2))
-#zL_pairs[0],zL_pairs[1]=np.repeat(zcens,len(Lcens)),np.tile(Lcens,len(zcens))
-#richness=np.zeros(len(zL_pairs[0]))
-zri_pairs=np.meshgrid(zcens,ricens)
-richness=np.zeros(np.shape(zri_pairs[0]))
-for i in np.arange(tsize):
-    for j in np.arange(tsize):
-        cur_bhz,cur_bhri=zri_pairs[0][i][j],zri_pairs[1][i][j]
-        richness[i][j]=len(np.where((np.abs(cur_bhz-bhz)<=0.5*zsize)&(np.abs(cur_bhri-bhri)<=0.5*risize))[0])
-plt.contour(zri_pairs[0],zri_pairs[1],richness)
+#rimin,rimax,zmin,zmax=np.min(medg_all-medr_all),np.max(medg_all-medr_all),np.min(medu_all-medg_all),np.max(medu_all-medg_all)
+#tsize=30
+#ribnds,zbnds=np.linspace(rimin,rimax,tsize+1),np.linspace(zmin,zmax,tsize+1)
+#zcens,ricens=0.5*(zbnds[:-1]+zbnds[1:]),0.5*(ribnds[:-1]+ribnds[1:])
+#zsize,risize=zcens[1]-zcens[0],ricens[1]-ricens[0]
+#zri_pairs=np.meshgrid(zcens,ricens)
+#richness=np.zeros(np.shape(zri_pairs[0]))
+#for i in np.arange(tsize):
+#    for j in np.arange(tsize):
+#        cur_bhz,cur_bhri=zri_pairs[0][i][j],zri_pairs[1][i][j]
+#        richness[i][j]=len(np.where((np.abs(cur_bhz-bhz)<=0.5*zsize)&(np.abs(cur_bhri-bhri)<=0.5*risize))[0])
+#plt.contour(zri_pairs[0],zri_pairs[1],richness)
 plt.scatter(medg[np.abs(cr['drop'])>1]-medr[np.abs(cr['drop'])>1],medu[np.abs(cr['drop'])>1]-medg[np.abs(cr['drop'])>1],color='green',s=clqsize/2)
 plt.scatter(medg[(np.abs(cr['drop'])>1)&(np.abs(cr['drop'])>1.5)]-medr[(np.abs(cr['drop'])>1)&(np.abs(cr['drop'])>1.5)],medu[(np.abs(cr['drop'])>1)&(np.abs(cr['drop'])>1.5)]-medg[(np.abs(cr['drop'])>1)&(np.abs(cr['drop'])>1.5)],color='magenta',s=clqsize/2+2)
 plt.scatter(medg[(np.abs(cr['drop'])>1)&(np.abs(cr['drop'])>2)]-medr[(np.abs(cr['drop'])>1)&(np.abs(cr['drop'])>2)],medu[(np.abs(cr['drop'])>1)&(np.abs(cr['drop'])>2)]-medg[(np.abs(cr['drop'])>1)&(np.abs(cr['drop'])>2)],color='red',s=clqsize/2+4)
@@ -220,18 +218,18 @@ plt.figure(1)
 plt.clf()
 bhL,bhug=medr_all-medi_all,medu_all-medg_all
 plt.scatter(bhug,bhL,color='k',s=2,edgecolor='None',marker='.')
-ugmin,ugmax,Lmin,Lmax=np.min(medu_all-medg_all),np.max(medu_all-medg_all),np.min(bhL),np.max(bhL)
-tsize=30
-ugbnds,Lbnds=np.linspace(ugmin,ugmax,tsize+1),np.linspace(Lmin,Lmax,tsize+1)
-Lcens,ugcens=0.5*(Lbnds[:-1]+Lbnds[1:]),0.5*(ugbnds[:-1]+ugbnds[1:])
-Lsize,ugsize=Lcens[1]-Lcens[0],ugcens[1]-ugcens[0]
-Lug_pairs=np.meshgrid(ugcens,Lcens)
-richness=np.zeros(np.shape(Lug_pairs[0]))
-for i in np.arange(tsize):
-    for j in np.arange(tsize):
-        cur_bhL,cur_bhug=Lug_pairs[0][i][j],Lug_pairs[1][i][j]
-        richness[i][j]=len(np.where((np.abs(cur_bhL-bhL)<=0.5*Lsize)&(np.abs(cur_bhug-bhug)<=0.5*ugsize))[0])
-plt.contour(Lug_pairs[0],Lug_pairs[1],richness,color='k')
+#ugmin,ugmax,Lmin,Lmax=np.min(medu_all-medg_all),np.max(medu_all-medg_all),np.min(bhL),np.max(bhL)
+#tsize=30
+#ugbnds,Lbnds=np.linspace(ugmin,ugmax,tsize+1),np.linspace(Lmin,Lmax,tsize+1)
+#Lcens,ugcens=0.5*(Lbnds[:-1]+Lbnds[1:]),0.5*(ugbnds[:-1]+ugbnds[1:])
+#Lsize,ugsize=Lcens[1]-Lcens[0],ugcens[1]-ugcens[0]
+#Lug_pairs=np.meshgrid(ugcens,Lcens)
+#richness=np.zeros(np.shape(Lug_pairs[0]))
+#for i in np.arange(tsize):
+#    for j in np.arange(tsize):
+#        cur_bhL,cur_bhug=Lug_pairs[0][i][j],Lug_pairs[1][i][j]
+#        richness[i][j]=len(np.where((np.abs(cur_bhL-bhL)<=0.5*Lsize)&(np.abs(cur_bhug-bhug)<=0.5*ugsize))[0])
+#plt.contour(Lug_pairs[0],Lug_pairs[1],richness,color='k')
 plt.scatter(medg[np.abs(cr['drop'])>1]-medr[np.abs(cr['drop'])>1],medu[np.abs(cr['drop'])>1]-medg[np.abs(cr['drop'])>1],color='green',s=clqsize/2)
 plt.scatter(medg[(np.abs(cr['drop'])>1)&(np.abs(cr['drop'])>1.5)]-medr[(np.abs(cr['drop'])>1)&(np.abs(cr['drop'])>1.5)],medu[(np.abs(cr['drop'])>1)&(np.abs(cr['drop'])>1.5)]-medg[(np.abs(cr['drop'])>1)&(np.abs(cr['drop'])>1.5)],color='magenta',s=clqsize/2+2)
 plt.scatter(medg[(np.abs(cr['drop'])>1)&(np.abs(cr['drop'])>2)]-medr[(np.abs(cr['drop'])>1)&(np.abs(cr['drop'])>2)],medu[(np.abs(cr['drop'])>1)&(np.abs(cr['drop'])>2)]-medg[(np.abs(cr['drop'])>1)&(np.abs(cr['drop'])>2)],color='red',s=clqsize/2+4)
