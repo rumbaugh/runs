2fa82d39b0e9ccb354742ef327d2a5956a2337c0
Nick Rumbaugh <takkyon13@gmail.com>
2017-03-24 09:05:45 -0700

    vingilot - Fri Mar 24 09:05:45 PDT 2017

--- run.3.24.17.1040.py
+++ run.3.24.17.1040.py
@@ -25,7 +25,7 @@ gmf_dr7=np.where(data['SDSSNAME']!='-1')[0]
 
 #cr=np.loadtxt('/home/rumbaugh/var_database/Y3A1/CLQ_candidates_DR7.3.8.17.dat',dtype={'names':('DBID','drop','S1','S2','S82','flag'),'formats':('|S24','f8','|S4','|S4','i8','i8')},skiprows=1)
 
-cr=np.loadtxt('/home/rumbaugh/var_database/Y3A1/max_mag_drop_DR7.3.8.17.dat',dtype={'names':('DBID','drop','Surv1','Surv2','S82','Baseline'),'formats':('|S32','f8','|S8','|S8','i8','f8')},skiprows=1)
+cr=np.loadtxt('/home/rumbaugh/var_database/Y3A1/max_mag_drop_DR7.3.22.17.dat',dtype={'names':('DBID','drop','Surv1','Surv2','S82','Baseline'),'formats':('|S32','f8','|S8','|S8','i8','f8')},skiprows=1)
 
 
 gmf=np.zeros(len(cr),dtype='i8')
