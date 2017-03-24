fe662f38bdc6f239e65e35ce32e23c0395932642
Nick Rumbaugh <takkyon13@gmail.com>
2017-03-24 14:42:57 -0700

    vingilot - Fri Mar 24 14:42:57 PDT 2017

--- run.3.24.17.1635.py
+++ run.3.24.17.1635.py
@@ -10,8 +10,8 @@ cr=np.loadtxt('%s/%s/SF.tab'%(DBdir,DBID),dtype={'names':('tau','SF1','SF2','SF3
 plt.figure(1)
 plt.clf()
 color_arr=['magenta','red','blue','green','cyan']
-for i in np.arange(1,6):plt.plot(cr['tau'],cr['SF%i'%i],lw=2,c=color_arr[i],label='SF%i'%i)
+for i in np.arange(1,6): plt.plot(cr['tau'],cr['SF%i'%i],lw=2,c=color_arr[i],label='SF%i'%i)
 plt.legend()
 plt.xlabel(r'$\Delta t$')
 plt.ylabel('Structure Function')
-plt.savefig('/home/rumbaugh/testplot_SF.png'
+plt.savefig('/home/rumbaugh/testplot_SF.png')
