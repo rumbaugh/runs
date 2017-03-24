79363b6d575e967a5f3dd3ea8200e1c44c424be8
Nick Rumbaugh <takkyon13@gmail.com>
2017-03-24 14:27:00 -0700

    vingilot - Fri Mar 24 14:27:00 PDT 2017

--- run.3.24.17.1610.py
+++ run.3.24.17.1610.py
@@ -36,5 +36,5 @@ for DBID in crmd['DBID']:
     mjd,mag,magerr=cr['MJD'],cr['MAG'],cr['MAGERR']
     SF1,SF2,SF3,SF4,SF5=CalcStructureFunction_eq14(mag,mjd,nbins=nbins),CalcStructureFunction_eq15(mag,mjd,magerr,nbins=nbins),CalcStructureFunction_eq16(mag,mjd,magerr,nbins=nbins),CalcStructureFunction_eq17(mag,mjd,nbins=nbins),CalcStructureFunction_eq18(mag,mjd,magerr,nbins=nbins)
     outcr=np.zeros((nbins,),dtype={'names':('tau','SF1','SF2','SF3','SF4','SF5'),'formats':('f8','f8','f8','f8','f8','f8')})
-    outcr['tau'],outcr['SF1'],outcr['SF2'],outcr['SF3'],outcr['SF4'],outcr['SF5']=SF1[:,0],SF1[:,1],SF2[:,1],SF3[:,1],SF4[:,1],SF5[:,1]
+    outcr['tau'],outcr['SF1'],outcr['SF2'],outcr['SF3'],outcr['SF4'],outcr['SF5']=SF1[0],SF1[1],SF2[1],SF3[1],SF4[1],SF5[1]
     np.savetxt('%s/%s/SF.tab'%(outputdir,DBID),outcr,fmt='%f %f %f %f %f %f')
