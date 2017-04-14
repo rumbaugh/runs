b781b282d270a9d5179c0209715fac117cbd381f
Nick Rumbaugh <takkyon13@gmail.com>
2017-04-14 14:55:58 -0700

    vingilot - Fri Apr 14 14:55:57 PDT 2017

--- run.4.14.17.1655.py
+++ run.4.14.17.1655.py
@@ -2,7 +2,7 @@ execfile('/home/rumbaugh/pythonscripts/plot_DB_lightcurves.py')
 
 crdb=np.loadtxt('/home/rumbaugh/var_database/Y3A1/database_index.dat',dtype={'names':('DBID','CID','thingid','sdr7id','MQrownum','SPrownum','SDSSNAME'),'formats':('|S64','i8','i8','|S24','i8','i8','|S64')})
 
-crmd=np.loadtxt('/home/rumbaugh/var_database/Y3A1/max_mag_drop_DR7.3.23.17.dat',dtype={'names':('DBID','drop','Surv1','Surv2','S82'),'formats':('|S32','f8','|S8','|S8','i8')},skiprows=1)
+crmd=np.loadtxt('/home/rumbaugh/var_database/Y3A1/max_mag_drop_DR7.old.3.23.17.dat',dtype={'names':('DBID','drop','Surv1','Surv2','S82'),'formats':('|S32','f8','|S8','|S8','i8')},skiprows=1)
 
 crmd=crmd[np.abs(crmd['drop'])>1]
 
