ece37f40e2eabfef026b463e0b8f1312bc3640be
Nick Rumbaugh <takkyon13@gmail.com>
2017-04-16 21:01:34 -0700

    vingilot - Sun Apr 16 21:01:34 PDT 2017

--- run.2.28.17.1830.py
+++ run.2.28.17.1830.py
@@ -14,4 +14,4 @@ cr['MAG_PSF'][np.isnan(cr['MAG_PSF'])]=0
 cr['MAG_PSF_ERROR'][np.isnan(cr['MAG_PSF_ERROR'])]=0
 cr['MAG_PSF'][cr['MAG_PSF']]=0
 np.savetxt('/home/rumbaugh/Eric_LC_Y3A1_abridged.tab',cr,header=('COADD_OBJECT_ID RA DEC MJD MAG_PSF MAG_PSF_ERROR BAND FLAGS'),fmt=('%i %f %f %f %f %f %s %i'),comments='')
-py.PrimaryHDU(cr).writeto('/home/rumbaugh/Eric_LC_Y3A1_abrdiged.fits',clobber=True)
+py.PrimaryHDU(cr).writeto('/home/rumbaugh/Eric_LC_Y3A1_abridged.fits',clobber=True)
