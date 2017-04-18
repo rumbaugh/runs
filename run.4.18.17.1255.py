6e6e9de8b36b3ea02baaabf9dd9b08ab000ed797
Nick Rumbaugh <takkyon13@gmail.com>
2017-04-18 12:30:48 -0700

    vingilot - Tue Apr 18 12:30:48 PDT 2017

--- run.4.18.17.1255.py
+++ run.4.18.17.1255.py
@@ -9,3 +9,9 @@ outcr['RA'],outcr['DEC'],outcr['Name']=crmi['RA'],crmi['DEC'],crmi['SDSS_NAME']
 outcr_radec[:,0],outcr_radec[:,1]=crmi['RA'],crmi['DEC']
 np.savetxt('/home/rumbaugh/radecname_forDEScutouts.4.18.17.csv',outcr,fmt='%f,%f,%s_DEScutout')
 np.savetxt('/home/rumbaugh/radec_forDEScutouts.4.18.17.csv',outcr_radec,fmt='%f,%f')
+
+FILE=open('/home/rumbaugh/runs/run.4.18.17.1255.sql','w')
+win=.2/60
+for i in range(0,len(outcr)):
+    FILE.write('SELECT RA,DEC,COADD_OBJECT_ID FROM Y3A1_COADD_OBJECT_SUMMARY where RA>%.6f AND RA<%.6f AND DEC> %.6f AND DEC<%.6f;\n'%(outcr['RA']-win,outcr['RA']+win,outcr['DEC']-win,outcr['DEC']+win))
+FILE.close()
