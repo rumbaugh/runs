fab5a8419aee924fb03b7e3b5fd40365c194dd1f
Nick Rumbaugh <takkyon13@gmail.com>
2017-04-19 19:23:30 -0700

    vingilot - Wed Apr 19 19:23:30 PDT 2017

--- run.4.19.17.1645.py
+++ run.4.19.17.1645.py
@@ -13,7 +13,7 @@ crd=np.loadtxt('/home/rumbaugh/var_database/Y3A1/DR7_full_magdiffs_wDBID.4.19.17
 drop,baseline=np.abs(crd['glo']-crd['ghi']),np.abs(crd['mjdlo']-crd['mjdhi'])/(1+crd['z'])
 matplotlib.rcParams['axes.linewidth']=3
 matplotlib.rcParams['font.size']=14
-evqdbids,longdbids=crd['DBID'][drop>1],crd['DBID'][baseline>3400]
+evqdbids,longdbids=crd['DBID'][drop>2],crd['DBID'][baseline>3400]
 plot_DB_lightcurves(evqdbids,'/home/rumbaugh/DR7_mag2+_lightcurves.4.19.17.pdf',DBdir='/home/rumbaugh/var_database/Y3A1',zoominband='g',calc_outliers=True,load_outliers=True,outlier_window=100,load_macleod=True,connectpoints=False)
-plot_DB_lightcurves(evqdbids,'/home/rumbaugh/DR7_baseline3400+_lightcurves.4.19.17.pdf',DBdir='/home/rumbaugh/var_database/Y3A1',zoominband='g',calc_outliers=True,load_outliers=True,outlier_window=100,load_macleod=True,connectpoints=False)
+plot_DB_lightcurves(longbids,'/home/rumbaugh/DR7_baseline3400+_lightcurves.4.19.17.pdf',DBdir='/home/rumbaugh/var_database/Y3A1',zoominband='g',calc_outliers=True,load_outliers=True,outlier_window=100,load_macleod=True,connectpoints=False)
 
