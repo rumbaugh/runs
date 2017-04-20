6ac2f5519a6c0c3f0ba5734a01495de88882087a
Nick Rumbaugh <takkyon13@gmail.com>
2017-04-20 15:01:05 -0700

    vingilot - Thu Apr 20 15:01:05 PDT 2017

--- run.4.20.17.1700.py
+++ run.4.20.17.1700.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 crc=np.loadtxt('/home/rumbaugh/radecname_forDEScutouts.4.20.17.csv',dtype={'names':('ra','dec','name'),'formats':('f8','f8','|S64')},delimiter=',')
-crdt=np.loadtxt('/home/rumbaugh/var_database/Y3A1/imagestamps/matched_37042c82-426f-888f-d77e3181f49b.csv',skiprows=1,delimiter=',',dtype={'names':('ra','dec','tile','fname'),'formats':('f8','f8','|S12','|S25')})
+crdt=np.loadtxt('/home/rumbaugh/var_database/Y3A1/imagestamps/matched_37042c82-db2b-426f-888f-d77e3181f49b.csv',skiprows=1,delimiter=',',dtype={'names':('ra','dec','tile','fname'),'formats':('f8','f8','|S12','|S25')})
 
 DBID=crc['name']
 for i in np.arange(len(DBID)):
