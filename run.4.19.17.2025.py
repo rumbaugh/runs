73a6510ed9544708cec755bd85d104aa776db15e
Nick Rumbaugh <takkyon13@gmail.com>
2017-04-19 18:25:32 -0700

    vingilot - Wed Apr 19 18:25:32 PDT 2017

---
+++ run.4.19.17.2025.py
@@ -0,0 +1,7 @@
+ntrials=10000000
+buff=100
+execfile('/home/rumbaugh/runs/run.4.19.17.1900.py')
+buff=300
+execfile('/home/rumbaugh/runs/run.4.19.17.1900.py')
+buff=600
+execfile('/home/rumbaugh/runs/run.4.19.17.1900.py')
