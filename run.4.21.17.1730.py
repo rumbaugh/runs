e6d30a3260bfd39155f72049ce9869799d1de87e
Nick Rumbaugh <takkyon13@gmail.com>
2017-04-21 15:34:11 -0700

    vingilot - Fri Apr 21 15:34:11 PDT 2017

---
+++ run.4.21.17.1730.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+cry=np.loadtxt('/home/rumbaugh/DR7_BH_Y3A1_MATCH_COADD_PARAMS.tab',dtype={'names':('SDSSNAME','CID','RA','DEC','mag_g','mag_r','mag_i','mag_z','mag_y','magerr_g','magerr_r','magerr_i','magerr_z','magerr_y','class_star'),'formats':('|S24','i8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8')},skiprows=1)
+crs=np.loadtxt('/home/rumbaugh/DR7_BH_Y3A1_MATCH_COADD_PARAMS.tab',dtype={'names':('class_star','CID','RA','DEC','mag_g','mag_r','mag_i','mag_z','mag_y'),'formats':('f8','i8','f8','f8','f8','f8','f8','f8','f8')},skiprows=1)
+
+
+matplotlib.rcParams['axes.linewidth']=3
+matplotlib.rcParams['font.size']=14
+
+plt.figure(1)
+plt.clf()
+plt.hist(cry['mag_g'])
+plt.xlabel('g-band Magnitude')
+plt.ylabel('Number of Object')
+plt.savefig('/home/rumbaugh/var_database/Y3A1/plots/magdist_DR7.hist.4.21.17.png')
+
+
+plt.figure(1)
+plt.clf()
+plt.hist(crs['mag_g'])
+plt.xlabel('g-band Magnitude')
+plt.ylabel('Number of Object')
+plt.savefig('/home/rumbaugh/var_database/Y3A1/plots/magdist_Y3A1_stars.hist.4.21.17.png')
