d99f31ea200e1ef9e4e244ec9c9cd1a1a818a22f
Nick Rumbaugh <takkyon13@gmail.com>
2017-04-20 19:43:48 -0700

    vingilot - Thu Apr 20 19:43:48 PDT 2017

--- run.4.19.17.1710.py
+++ run.4.19.17.1710.py
@@ -114,10 +114,11 @@ fig.clf()
 plt.clf()
 plt.rc('axes',linewidth=2)
 ax1=fig.add_subplot(1,1,1)
+ax2=ax1.twinx()
 pos1 = ax1.get_position() # get the original position 
 pos2 = [pos1.x0, pos1.y0 + 0.03,  pos1.width, pos1.height] 
 ax1.set_position(pos2)
-ax2=ax1.twinx()
+ax2.set_position(pos2)
 ax1.tick_params(which='major',length=12,width=3,labelsize=17)
 ax1.tick_params(which='minor',length=6,width=2,labelsize=17)
 ax2.tick_params(which='major',length=12,width=3,labelsize=17)
