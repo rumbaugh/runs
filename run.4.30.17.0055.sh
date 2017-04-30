591ed519cac6ac1c0e58b078d23660603247a410
Nick Rumbaugh <takkyon13@gmail.com>
2017-04-29 22:56:41 -0700

    vingilot - Sat Apr 29 22:56:40 PDT 2017

---
+++ run.4.30.17.0055.sh
@@ -0,0 +1,2 @@
+sed -i -e 's/"/ /g' /home/rumbaugh/dr7_bh_lightcurve_entries_y3a1_closechanges_wspread.tab
+sed -i -e 's/"/ /g' /home/rumbaugh/dr7_bh_lightcurve_entries_y3a1_wspread.tab
---
+++ run.4.30.17.0055.sh~
@@ -0,0 +1,1 @@
+sed -i -e 's/"/ /g' /home/rumbaugh/dr7_bh_lightcurve_entries_y3a1_closechanges.tab
