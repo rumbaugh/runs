21bda81c23be3c6afd8b17cad2250e4649186dcd
Nick Rumbaugh <takkyon13@gmail.com>
2017-04-30 15:30:46 -0700

    vingilot - Sun Apr 30 15:30:46 PDT 2017

---
+++ run.4.30.17.1725.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+cr=np.loadtxt('/home/rumbaugh/var_database/Y3A1/DR7_full_magdiffs_wDBID.4.28.17.tab',dtype={'names':('RA','DEC','z','mjdlo','glo','siglo','flaglo','mjdhi','ghi','sighi','flaghi','RA_DES','DEC_DES','DBID'),'formats':('f8','f8','f8','f8','f8','f8','i8','f8','f8','f8','i8','f8','f8','|S24')})
+crs=np.loadtxt('/home/rumbaugh/var_database/Y3A1/DR7.evq_spreads.4.30.17.dat')
+drop=np.abs(cr['glo']-cr['ghi'])
+
+plt.figure(1)
+plt.clf()
+plt.hist(crs.flatten(),lw=2,color='k',normed=True)
+plt.hist(crs[drop>1].flatten(),lw=2,color='r',edgecolor='r',facecolor='None',normed=True)
+plt.xlabel('Spread')
+plt.ylabel('Normalized Number of Objects')
+plt.savefig('/home/rumbaugh/spreadcheck.evqs.4.30.17.png')
