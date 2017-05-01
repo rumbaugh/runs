66d6cc6df4a61694a6b4e9ea5c605995065a0c56
Nick Rumbaugh <takkyon13@gmail.com>
2017-05-01 14:47:01 -0700

    vingilot - Mon May  1 14:47:01 PDT 2017

--- run.5.1.17.1550.py
+++ run.5.1.17.1550.py
@@ -5,9 +5,14 @@ import matplotlib.pyplot as plt
 
 cr=np.loadtxt('/home/rumbaugh/var_database/Y3A1/DR7_full_magdiffs_wDBID.4.28.17.tab',dtype={'names':('RA','DEC','z','mjdlo','glo','siglo','flaglo','mjdhi','ghi','sighi','flaghi','RA_DES','DEC_DES','DBID'),'formats':('f8','f8','f8','f8','f8','f8','i8','f8','f8','f8','i8','f8','f8','|S24')})
 crs=np.loadtxt('/home/rumbaugh/var_database/Y3A1/DR7.evq_spreads.4.30.17.dat')
+drop=np.abs(cr['glo']-cr['ghi'])
+crevq=crs[drop>1]
 spreads=np.append(crs[:,0],crs[:,1])
 spreaderrs=np.append(crs[:,2],crs[:,3])
-drop=np.abs(cr['glo']-cr['ghi'])
+evqspreads=np.append(crevq[:,0],crevq[:,1])
+evqspreaderrs=np.append(crevq[:,2],crevq[:,3])
+
+
 
 xdummy=np.linspace(0.0000001,0.1,1000)
 ydummy=np.sqrt(0.003**2+4*xdummy**2)
@@ -35,3 +40,28 @@ plt.ylabel('Spread')
 plt.xlim(0.00003,0.054)
 plt.ylim(-0.009,0.024)
 plt.savefig('/home/rumbaugh/spread_ps_check.loglog.5.1.17.png')
+
+
+plt.figure(1)
+plt.clf()
+plt.scatter(evqspreaderrs,evqspreads,color='b',s=6,edgecolor='None',alpha=0.2)
+plt.plot(xdummy,ydummy,color='r',lw=2,ls='dashed')
+plt.plot(xdummy,-ydummy,color='r',lw=2,ls='dashed')
+plt.xlabel('Spread Error')
+plt.ylabel('Spread')
+plt.xlim(0,0.054)
+plt.ylim(-0.009,0.024)
+plt.savefig('/home/rumbaugh/spreadevq_ps_check.5.1.17.png')
+
+glog=np.where(spreaderrs>0)[0]
+plt.figure(1)
+plt.clf()
+plt.xscale('log')
+plt.scatter(evqspreaderrs[glog],evqspreads[glog],color='b',s=6,edgecolor='None',alpha=0.2)
+plt.plot(xdummy,ydummy,color='r',lw=2,ls='dashed')
+plt.plot(xdummy,-ydummy,color='r',lw=2,ls='dashed')
+plt.xlabel('Spread Error')
+plt.ylabel('Spread')
+plt.xlim(0.00003,0.054)
+plt.ylim(-0.009,0.024)
+plt.savefig('/home/rumbaugh/spreadevq_ps_check.loglog.5.1.17.png')
