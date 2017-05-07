fedfd08a9c6511b3190d410d16d04c3b1179a28c
Nick Rumbaugh <takkyon13@gmail.com>
2017-05-06 19:04:56 -0700

    vingilot - Sat May  6 19:04:56 PDT 2017

---
+++ run.5.6.17.2105.py
@@ -0,0 +1,7 @@
+ntrials=10000000
+buff=100
+execfile('/home/rumbaugh/runs/run.5.6.17.1955.py')
+buff=300
+execfile('/home/rumbaugh/runs/run.5.6.17.1955.py')
+buff=600
+execfile('/home/rumbaugh/runs/run.5.6.17.1955.py')
