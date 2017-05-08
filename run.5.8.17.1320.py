ededadb7bf90be95fc806dc04f652ac2119ffb7b
Nick Rumbaugh <takkyon13@gmail.com>
2017-05-08 14:31:35 -0700

    vingilot - Mon May  8 14:31:35 PDT 2017

--- run.5.8.17.1320.py
+++ run.5.8.17.1320.py
@@ -2,6 +2,9 @@ import numpy as np
 ntrials=10000000
 buffcrs={}
 for buff in [100,300,600]:
-    buffcrs[buff]=np.loadtxt('/home/rumbaugh/mock_EVQs.buff_%i.trials_%i.5.6.17.dat'%(buff,ntrials),dtype={'names':('baseline_RFredshift','anchor_epoch','second_epoch','detected','firstspec','secondspec','lowepoch','CLQ_detected'),'formats':('|S24','f8','f8','i8','f8','f8','i8','i8')})
-    print 'Buffer=%i:\nTotal CLQ detection fraction: %.5f\nEVQ CLQ detection fraction: %.5f'%(buff,np.count_nonzero(buffcrs[buff]['CLQ_detected'])*1./ntrials,np.count_nonzero(buffcrs[buff]['CLQ_detected'][buffcrs[buff]['detected']>0])*1./len(buffcrs[buff]['CLQ_detected'][buffcrs[buff]['detected']>0]))
+    buffcrs[buff]=np.loadtxt('/home/rumbaugh/mock_EVQs.buff_%i.trials_%i.fixed.5.6.17.dat'%(buff,ntrials),dtype={'names':('baseline_RF','redshift','anchor_epoch','second_epoch','detected','firstspec','secondspec','lowepoch','CLQ_detected'),'formats':('f8','f8','f8','f8','i8','f8','f8','i8','i8')})
+    print 'Buffer=%i:\nTotal CLQ detection fraction: %.5f\n'%(buff,np.count_nonzero(buffcrs[buff]['CLQ_detected'])*1./ntrials)
+    for LB,UB in zip(np.arange(0,6000,1000),[1000,2000,3000,4000,5000,10000]):
+        g=np.where((buffcrs[buff]['baseline_
+    print 'EVQ CLQ detection fraction: %.5f'%(np.count_nonzero(buffcrs[buff]['CLQ_detected'][buffcrs[buff]['detected']>0])*1./len(buffcrs[buff]['CLQ_detected'][buffcrs[buff]['detected']>0]))
 
