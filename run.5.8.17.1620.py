8b3be4bc01119fc17cf7c1cc6fd2b63bd4e86005
Nick Rumbaugh <takkyon13@gmail.com>
2017-05-08 15:17:31 -0700

    vingilot - Mon May  8 15:17:31 PDT 2017

--- run.5.8.17.1620.py
+++ run.5.8.17.1620.py
@@ -5,7 +5,7 @@ buffcrs0={}
 for buff in [100,300,600]:
     buffcrs[buff]=np.loadtxt('/home/rumbaugh/mock_EVQs.buff_%i.trials_%i.5.6.17.dat'%(buff,ntrials),dtype={'names':('baseline_RFredshift','anchor_epoch','second_epoch','detected','firstspec','secondspec','lowepoch','CLQ_detected'),'formats':('|S24','f8','f8','i8','f8','f8','i8','i8')})
     buffcrs0[buff]=np.loadtxt('/home/rumbaugh/mock_EVQs.buff_%i.trials_%i.baseline.redshift.5.6.17.dat'%(buff,ntrials),dtype={'names':('baseline_RF','baselinedecredshift','redshiftdec'),'formats':('i8','i8','i8')},delimiter='.')
-    baselinedec=np.int(np.floor(buffcrs0[buff]['baselinedecredshift']/10.))
+    baselinedec=np.array(np.floor(buffcrs0[buff]['baselinedecredshift']/10.),dtype='i8')
     redshift=buffcrs0[buff]['baselinedecredshift']-baselindec
     print buff,len(buffcrs0[buff][buffcrs0[buff]['redshiftdec']==987654321099]),len(buffcrs0[buff][buffcrs0[buff]['redshiftdec']==987654321098])
     buffcrs0[buff]['redshiftdec'][buffcrs0[buff]['redshiftdec']==987654321099]=0
