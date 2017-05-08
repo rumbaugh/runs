3c5269eb7d75940a9a29a6b229352fb939293011
Nick Rumbaugh <takkyon13@gmail.com>
2017-05-08 14:44:46 -0700

    vingilot - Mon May  8 14:44:46 PDT 2017

--- run.5.8.17.1620.py
+++ run.5.8.17.1620.py
@@ -3,7 +3,7 @@ ntrials=10000000
 buffcrs={}
 for buff in [100,300,600]:
     buffcrs[buff]=np.loadtxt('/home/rumbaugh/mock_EVQs.buff_%i.trials_%i.5.6.17.dat'%(buff,ntrials),dtype={'names':('baseline_RFredshift','anchor_epoch','second_epoch','detected','firstspec','secondspec','lowepoch','CLQ_detected'),'formats':('|S24','f8','f8','i8','f8','f8','i8','i8')})
-    buffcrs0[buff]=np.loadtxt('/home/rumbaugh/mock_EVQs.buff_%i.trials_%i.5.6.17.dat'%(buff,ntrials),dtype={'names':('baseline_RF','baselinedecredshift','redshiftdec'),'formats':('i8','i8','i8')},delimiter='.')
+    buffcrs0[buff]=np.loadtxt('/home/rumbaugh/mock_EVQs.buff_%i.trials_%i.baseline.redshift.5.6.17.dat'%(buff,ntrials),dtype={'names':('baseline_RF','baselinedecredshift','redshiftdec'),'formats':('i8','i8','i8')},delimiter='.')
     baselinedec=np.int(np.floor(buffcrs0[buff]['baselinedecredshift']/10.))
     redshift=buffcrs0[buff]['baselinedecredshift']-baselindec
     outcr=np.zeros((ntrials,),dtype={'names':('baseline_RF','baseline_RFdec','redshift','redshiftdec','anchor_epoch','second_epoch','detected','firstspec','secondspec','lowepoch','CLQ_detected'),'formats':('i8','i8','i8','i8','f8','f8','i8','f8','f8','i8','i8')})
--- run.5.8.17.1635.py
+++ run.5.8.17.1635.py
@@ -3,4 +3,6 @@ ntrials=10000000
 buffcrs={}
 for buff in [100,300,600]:
     buffcrs[buff]=np.loadtxt('/home/rumbaugh/mock_EVQs.buff_%i.trials_%i.5.6.17.dat'%(buff,ntrials),dtype={'names':('baseline_RFredshift','anchor_epoch','second_epoch','detected','firstspec','secondspec','lowepoch','CLQ_detected'),'formats':('|S24','f8','f8','i8','f8','f8','i8','i8')})
-    np.savetxt('/home/rumbaugh/mock_EVQs.buff_%i.trials_%i.baseline.redshift.5.6.17.dat'%(buff,ntrials),buffcrs[buff]['baseline_RFredshift'],fmt='%s')
+    outcr=np.zeros((ntrials,),dtype={'names':('a','b','c'),'formats':('|S24','i8','i8')})
+    outcr['a'],outcr['b'],outcr['c']=buffcrs[buff]['baseline_RFredshift'],987654321099,987654321098
+    np.savetxt('/home/rumbaugh/mock_EVQs.buff_%i.trials_%i.baseline.redshift.5.6.17.dat'%(buff,ntrials),outcr,fmt='%s.%i.%i')
