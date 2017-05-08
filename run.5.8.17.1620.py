f734d137783070a36a6bc5978062b4e1b50df3fe
Nick Rumbaugh <takkyon13@gmail.com>
2017-05-08 15:14:15 -0700

    vingilot - Mon May  8 15:14:15 PDT 2017

--- run.5.8.17.1620.py
+++ run.5.8.17.1620.py
@@ -1,6 +1,7 @@
 import numpy as np
 ntrials=10000000
 buffcrs={}
+buffcrs0={}
 for buff in [100,300,600]:
     buffcrs[buff]=np.loadtxt('/home/rumbaugh/mock_EVQs.buff_%i.trials_%i.5.6.17.dat'%(buff,ntrials),dtype={'names':('baseline_RFredshift','anchor_epoch','second_epoch','detected','firstspec','secondspec','lowepoch','CLQ_detected'),'formats':('|S24','f8','f8','i8','f8','f8','i8','i8')})
     buffcrs0[buff]=np.loadtxt('/home/rumbaugh/mock_EVQs.buff_%i.trials_%i.baseline.redshift.5.6.17.dat'%(buff,ntrials),dtype={'names':('baseline_RF','baselinedecredshift','redshiftdec'),'formats':('i8','i8','i8')},delimiter='.')
