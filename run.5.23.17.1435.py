4a9a254ca308d1138fe6b80a62ac333196b5ded6
Nick Rumbaugh <takkyon13@gmail.com>
2017-05-23 12:42:33 -0700

    vingilot - Tue May 23 12:42:33 PDT 2017

--- run.5.23.17.1435.py
+++ run.5.23.17.1435.py
@@ -4,7 +4,7 @@ import pyfits as py
 hdu=py.open('/home/rumbaugh/var_database/Y3A1/old_masterfile.fits')
 data=hdu[1].data
 
-tids,inds=np.unique(data['DR13_thingid'])
+tids,inds=np.unique(data['DR13_thingid'],True)
 tids,inds=tids[tids>0],inds[tids>0]
 outcr=np.zeros((len(tids),),dtype={'names':('inds','tids'),'formats':('i8','i8')})
 outcr['inds'],outcr['tids']=inds,tids
