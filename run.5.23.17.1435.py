49f38d38876e128035cd15d5e4895b10422a9fc3
Nick Rumbaugh <takkyon13@gmail.com>
2017-05-23 15:53:12 -0700

    vingilot - Tue May 23 15:53:11 PDT 2017

--- run.5.23.17.1435.py
+++ run.5.23.17.1435.py
@@ -4,11 +4,9 @@ import pyfits as py
 hdu=py.open('/home/rumbaugh/var_database/Y3A1/old_masterfile.fits')
 data=hdu[1].data
 
-data=data[(data['SDSS_NAME']!='-1')|(data['SP_ROWNUM']>-1)]
-
 tids,inds=np.unique(data['DR13_thingid'],True)
 tids,inds=tids[tids>0],inds[tids>0]
 outcr=np.zeros((len(tids),),dtype={'names':('inds','tids'),'formats':('i8','i8')})
 outcr['inds'],outcr['tids']=inds,tids
 
-np.savetxt('/home/rumbaugh/DR13_thingids.noMQ.tab',outcr,fmt='%i %i',header='IND THINGID',comments='')
+np.savetxt('/home/rumbaugh/DR13_things.tab',outcr,fmt='%i %i',header='IND THINGID',comments='')
--- run.5.23.17.1750.py
+++ run.5.23.17.1750.py
@@ -4,7 +4,7 @@ import pyfits as py
 hdu=py.open('/home/rumbaugh/var_database/Y3A1/old_masterfile.fits')
 data=hdu[1].data
 
-data=data[(data['SDSS_NAME']!='-1')|(data['SP_ROWNUM']>-1)]
+data=data[(data['SDSSNAME']!='-1')|(data['SP_ROWNUM']>-1)]
 
 tids,inds=np.unique(data['DR13_thingid'],True)
 tids,inds=tids[tids>0],inds[tids>0]
