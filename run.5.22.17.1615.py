a7cf3813f1b91792144e9170f50daef778044c57
Nick Rumbaugh <takkyon13@gmail.com>
2017-05-24 15:50:49 -0700

    vingilot - Wed May 24 15:50:49 PDT 2017

--- run.5.22.17.1615.py
+++ run.5.22.17.1615.py
@@ -131,8 +131,8 @@ for cid,MQrn,SPrn,SDSSNAME,imi,TILENAME in zip(crmim['COADD_OBJECTS_ID'],crmim['
     except IOError:
         crout=np.zeros(len(cr))
     try:
-        crmac=np.loadtxt('%s/%s/Macleod_LC.tab'%(DB_path,DBID),dtype={'names':('DatabaseID','RA','DEC','MJD','BAND','MAG','MAGERR','FLAG','UNIMAG'),'formats':('|S24','f8','f8','f8','|S4','f8','f8','i8','f8')})
-        outcrmac=np.zeros((len(crmac),),dtype={'names':('Survey','SurveyCoaddID','SurveyObjectID','RA','DEC','MJD','TAG','BAND','MAGTYPE','MAG','MAGERR','FLAG','UNIMAG'),'formats':('|S20','|S20','|S20','f8','f8','f8','|S20','|S12','|S12','f8','f8','i8','f8')})
+        crmac=np.loadtxt('%s/%s/Macleod_LC.tab'%(DB_path,DBID),dtype={'names':('DatabaseID','RA','DEC','MJD','BAND','MAG','MAGERR','FLAG'),'formats':('|S24','f8','f8','f8','|S4','f8','f8','i8')})
+        outcrmac=np.zeros((len(crmac),),dtype={'names':('Survey','SurveyCoaddID','SurveyObjectID','RA','DEC','MJD','TAG','BAND','MAGTYPE','MAG','MAGERR','FLAG'),'formats':('|S20','|S20','|S20','f8','f8','f8','|S20','|S12','|S12','f8','f8','i8')})
         outcrmac['Survey'],outcrmac['SurveyCoaddID'],outcrmac['RA'],outcrmac['DEC'],outcrmac['MJD'],outcrmac['TAG'],outcrmac['BAND'],outcrmac['MAGTYPE'],outcrmac['MAG'],outcrmac['MAGERR'],outcrmac['FLAG']='SDSS',crmac['DatabaseID'],crmac['RA'],crmac['DEC'],crmac['MJD'],'MACLEOD',crmac['BAND'],'PSF',crmac['MAG'],crmac['MAGERR'],crmac['FLAG']
         try:
             croutmac=np.loadtxt('%s/%s/outliers_Macleod.tab'%(DB_path,DBID),dtype='i8')
