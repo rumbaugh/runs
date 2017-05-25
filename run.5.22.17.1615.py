9c23d99615b3da8c3070a3398184248481bc2305
Nick Rumbaugh <takkyon13@gmail.com>
2017-05-25 12:28:41 -0700

    vingilot - Thu May 25 12:28:41 PDT 2017

--- run.5.22.17.1615.py
+++ run.5.22.17.1615.py
@@ -129,7 +129,7 @@ for cid,MQrn,SPrn,SDSSNAME,imi,TILENAME in zip(crmim['COADD_OBJECTS_ID'],crmim['
         #    outcr['OUTLIER']=crout
         #    outcr['OUTLIER'][(crout==0)&(outcr['BAND']=='g')]=-1
     except IOError:
-        crout=np.zeros(len(cr))
+        crout=np.zeros(len(cr),dtype='i8')
     try:
         crmac=np.loadtxt('%s/%s/Macleod_LC.tab'%(DB_path,DBID),dtype={'names':('DatabaseID','RA','DEC','MJD','BAND','MAG','MAGERR','FLAG'),'formats':('|S24','f8','f8','f8','|S4','f8','f8','i8')})
         outcrmac=np.zeros((len(crmac),),dtype={'names':('Survey','SurveyCoaddID','SurveyObjectID','RA','DEC','MJD','TAG','BAND','MAGTYPE','MAG','MAGERR','FLAG'),'formats':('|S20','|S20','|S20','f8','f8','f8','|S20','|S12','|S12','f8','f8','i8')})
