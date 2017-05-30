cde4be6705ef7d1a92f6632d25a4bd3ca9f550ad
Nick Rumbaugh <takkyon13@gmail.com>
2017-05-30 14:35:57 -0700

    vingilot - Tue May 30 14:35:57 PDT 2017

--- run.5.29.17.2240.py
+++ run.5.29.17.2240.py
@@ -27,40 +27,61 @@ data=data[dataid]
 
 plt.figure(1)
 plt.clf()
-plt.hist(data['EW_BROAD_HB'],range=(0.5,3),bins=25,normed=True,facecolor='None',edgecolor='k',lw=2)
-plt.hist(cdata['EW_BROAD_HB'],range=(0.5,3),bins=25,normed=True,facecolor='None',edgecolor='green',lw=2)
+plt.hist(np.log10(data['EW_OIII_4959']),range=(0.5,3),bins=25,normed=True,facecolor='None',edgecolor='k',lw=2)
+plt.hist(np.log10(cdata['EW_OIII_4959']),range=(0.5,3),bins=25,normed=True,facecolor='None',edgecolor='green',lw=2)
 plt.xlabel(r'H$\beta$')
-plt.savefig('/home/rumbaugh/var_database/Y3A1/spectral_plots.HB.5.29.17.png')
+plt.savefig('/home/rumbaugh/var_database/Y3A1/plots/spectral_plots.OIII_4959.5.29.17.png')
+
+print 'OIII_4959'
+print KStest(data['EW_OIII_4959'][(np.log10(data['EW_OIII_4959'])>=0.5)&(np.log10(data['EW_OIII_4959'])<=3)],cdata['EW_OIII_4959'][(np.log10(cdata['EW_OIII_4959'])>=0.5)&(np.log10(cdata['EW_OIII_4959'])<=3)])
+
+
+plt.figure(1)
+plt.clf()
+plt.hist(np.log10(data['EW_OIII_5007']),range=(0.5,3),bins=25,normed=True,facecolor='None',edgecolor='k',lw=2)
+plt.hist(np.log10(cdata['EW_OIII_5007']),range=(0.5,3),bins=25,normed=True,facecolor='None',edgecolor='green',lw=2)
+plt.xlabel(r'H$\beta$')
+plt.savefig('/home/rumbaugh/var_database/Y3A1/plots/spectral_plots.OIII_5007.5.29.17.png')
+
+print 'OIII_5007'
+print KStest(data['EW_OIII_5007'][(np.log10(data['EW_OIII_5007'])>=0.5)&(np.log10(data['EW_OIII_5007'])<=3)],cdata['EW_OIII_5007'][(np.log10(cdata['EW_OIII_5007'])>=0.5)&(np.log10(cdata['EW_OIII_5007'])<=3)])
+
+plt.figure(1)
+plt.clf()
+plt.hist(np.log10(data['EW_BROAD_HB']),range=(0.5,3),bins=25,normed=True,facecolor='None',edgecolor='k',lw=2)
+plt.hist(np.log10(cdata['EW_BROAD_HB']),range=(0.5,3),bins=25,normed=True,facecolor='None',edgecolor='green',lw=2)
+plt.xlabel(r'H$\beta$')
+plt.savefig('/home/rumbaugh/var_database/Y3A1/plots/spectral_plots.HB.5.29.17.png')
 
 print 'Hbeta'
-print KStest(data['EW_BROAD_HB'],cdata['EW_BROAD_HB'])
+print KStest(data['EW_BROAD_HB'][(np.log10(data['EW_BROAD_HB'])>=0.5)&(np.log10(data['EW_BROAD_HB'])<=3)],cdata['EW_BROAD_HB'][(np.log10(cdata['EW_BROAD_HB'])>=0.5)&(np.log10(cdata['EW_BROAD_HB'])<=3)])
 
 plt.figure(1)
 plt.clf()
-plt.hist(data['EW_MGII']/100.,range=(0.5,3.5),bins=30,normed=True,facecolor='None',edgecolor='k',lw=2)
-plt.hist(cdata['EW_MGII']/100.,range=(0.5,3.5),bins=30,normed=True,facecolor='None',edgecolor='green',lw=2)
+plt.hist(np.log10(data['EW_MGII']),range=(0.5,3.5),bins=30,normed=True,facecolor='None',edgecolor='k',lw=2)
+plt.hist(np.log10(cdata['EW_MGII']),range=(0.5,3.5),bins=30,normed=True,facecolor='None',edgecolor='green',lw=2)
 plt.xlabel('MgII')
-plt.savefig('/home/rumbaugh/var_database/Y3A1/spectral_plots.MgII.5.29.17.png')
+plt.savefig('/home/rumbaugh/var_database/Y3A1/plots/spectral_plots.MgII.5.29.17.png')
 
 print 'MgII'
-print KStest(data['EW_MGII']/100.,cdata['EW_MGII']/100.)
+print KStest(data['EW_MGII'][(np.log10(data['EW_MGII'])>=0.5)&(np.log10(data['EW_MGII'])<=3.5)],cdata['EW_MGII'][(np.log10(cdata['EW_MGII'])>=0.5)&(np.log10(cdata['EW_MGII'])<=3.5)])
 
 plt.figure(1)
 plt.clf()
-plt.hist(data['EW_CIV'],range=(0,4),bins=40,normed=True,facecolor='None',edgecolor='k',lw=2)
-plt.hist(cdata['EW_CIV'],range=(0,4),bins=40,normed=True,facecolor='None',edgecolor='green',lw=2)
+plt.hist(np.log10(data['EW_CIV']),range=(0,4),bins=40,normed=True,facecolor='None',edgecolor='k',lw=2)
+plt.hist(np.log10(cdata['EW_CIV']),range=(0,4),bins=40,normed=True,facecolor='None',edgecolor='green',lw=2)
 plt.xlabel('CIV')
-plt.savefig('/home/rumbaugh/var_database/Y3A1/spectral_plots.CIV.5.29.17.png')
+plt.savefig('/home/rumbaugh/var_database/Y3A1/plots/spectral_plots.CIV.5.29.17.png')
 
 print 'CIV'
-print KStest(data['EW_CIV'],cdata['EW_CIV'])
+print KStest(data['EW_CIV'][(np.log10(data['EW_CIV'])>=0)&(np.log10(data['EW_CIV'])<=4)],cdata['EW_CIV'][(np.log10(cdata['EW_CIV'])>=0)&(np.log10(cdata['EW_CIV'])<=4)])
 
 plt.figure(1)
 plt.clf()
 plt.hist(data['LOGEDD_RATIO'],range=(-3,1),bins=40,normed=True,facecolor='None',edgecolor='k',lw=2)
 plt.hist(cdata['LOGEDD_RATIO'],range=(-3,1),bins=40,normed=True,facecolor='None',edgecolor='green',lw=2)
 plt.xlabel(r'log $L/L_{edd}$')
-plt.savefig('/home/rumbaugh/var_database/Y3A1/spectral_plots.Ledd.5.29.17.png')
+plt.savefig('/home/rumbaugh/var_database/Y3A1/plots/spectral_plots.Ledd.5.29.17.png')
 
 print 'Ledd'
-print KStest(data['LOGEDD_RATIO'],cdata['LOGEDD_RATIO'])
+print KStest(data['LOGEDD_RATIO'][(data['LOGEDD_RATIO']>=-3)&(data['LOGEDD_RATIO']<=1)],cdata['LOGEDD_RATIO'][(cdata['LOGEDD_RATIO']>=-3)&(cdata['LOGEDD_RATIO']<=1)])
---
+++ run.5.30.17.1625.py
@@ -0,0 +1,60 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+crd=np.loadtxt('/home/rumbaugh/var_database/Y3A1/DR7_full_magdiffs_wDBID.4.30.17.tab',dtype={'names':('RA','DEC','z','mjdlo','glo','siglo','flaglo','mjdhi','ghi','sighi','flaghi','RA_DES','DEC_DES','DBID','y3a1_mag_auto_g','ilo','ihi','rlo','rhi'),'formats':('f8','f8','f8','f8','f8','f8','i8','f8','f8','f8','i8','f8','f8','|S24','f8','f8','f8','f8','f8')})
+
+gswap=np.where(crd['mjdlo']>crd['mjdhi'])[0]
+if len(gswap)>0:
+    crd['glo'][gswap],crd['ghi'][gswap],crd['ilo'][gswap],crd['ihi'][gswap],crd['mjdlo'][gswap],crd['mjdhi'][gswap],crd['rlo'][gswap],crd['rhi'][gswap]=crd['ghi'][gswap],crd['glo'][gswap],crd['ihi'][gswap],crd['ilo'][gswap],crd['mjdhi'][gswap],crd['mjdlo'][gswap],crd['rhi'][gswap],crd['rlo'][gswap]
+
+crd=crd[(crd['ilo']>0)&(crd['ihi']>0)&(crd['rlo']>0)&(crd['rhi']>0)]
+
+gdrop=crd['ghi']-crd['glo']
+gihi,gilo,grhi,grlo=crd['ghi']-crd['ihi'],crd['glo']-crd['ilo'],crd['ghi']-crd['rhi'],crd['glo']-crd['rlo']
+gidrop,grdrop=gihi-gilo,grhi-grlo
+
+matplotlib.rcParams['axes.linewidth']=3
+matplotlib.rcParams['font.size']=14
+
+yticks_minor=np.arange(-1.25,1.75,0.5)
+xticks_minor=np.arange(-1.5,2.5,1)
+
+fig=plt.figure(1)
+fig.clf()
+plt.clf()
+ax1=fig.add_subplot(1,1,1)
+pos1 = ax1.get_position() # get the original position 
+pos2 = [pos1.x0, pos1.y0 + 0.03,  pos1.width, pos1.height] 
+ax1.set_position(pos2)
+ax1.tick_params(which='major',length=9,width=3,labelsize=17)
+ax1.tick_params(which='minor',length=5,width=2,labelsize=17)
+ax1.scatter(gdrop,gidrop,color='k',edgecolor='None',s=6)
+ax1.scatter(gdrop[np.abs(gdrop)>1],gidrop[np.abs(gdrop)>1],color='r',edgecolor='None',s=6)
+ax1.set_xlim(-2.4,2.4)
+ax1.set_ylim(-1.4,1.4)
+ax1.set_xlabel(r'$\Delta g$',fontsize=20)
+ax1.set_ylabel(r'$\Delta \left(g-i\right)$',fontsize=20)
+ax1.set_xticks(xticks_minor,minor=True)
+ax1.set_yticks(yticks_minor,minor=True)
+fig.savefig('/home/rumbaugh/var_database/Y3A1/plots/g_vs_g-i.EVQs.png')
+
+fig=plt.figure(1)
+fig.clf()
+plt.clf()
+ax1=fig.add_subplot(1,1,1)
+pos1 = ax1.get_position() # get the original position 
+pos2 = [pos1.x0, pos1.y0 + 0.03,  pos1.width, pos1.height] 
+ax1.set_position(pos2)
+ax1.tick_params(which='major',length=9,width=3,labelsize=17)
+ax1.tick_params(which='minor',length=5,width=2,labelsize=17)
+ax1.scatter(gdrop,grdrop,color='k',edgecolor='None',s=6)
+ax1.scatter(gdrop[np.abs(gdrop)>1],grdrop[np.abs(gdrop)>1],color='r',edgecolor='None',s=6)
+ax1.set_xlim(-2.4,2.4)
+ax1.set_ylim(-1.4,1.4)
+ax1.set_xlabel(r'$\Delta g$',fontsize=20)
+ax1.set_ylabel(r'$\Delta \left(g-r\right)$',fontsize=20)
+ax1.set_xticks(xticks_minor,minor=True)
+ax1.set_yticks(yticks_minor,minor=True)
+fig.savefig('/home/rumbaugh/var_database/Y3A1/plots/g_vs_g-r.EVQs.png')
