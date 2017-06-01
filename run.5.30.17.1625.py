e85d53a6c5a2618eb89a6ba530c64934a8a25525
Nick Rumbaugh <takkyon13@gmail.com>
2017-06-01 15:00:58 -0700

    vingilot - Thu Jun  1 15:00:58 PDT 2017

--- run.5.30.17.1625.py
+++ run.5.30.17.1625.py
@@ -12,6 +12,7 @@ if len(gswap)>0:
 crd=crd[(crd['ilo']>0)&(crd['ihi']>0)&(crd['rlo']>0)&(crd['rhi']>0)]
 
 gdrop=crd['ghi']-crd['glo']
+idrop=crd['ihi']-crd['ilo']
 gihi,gilo,grhi,grlo=crd['ghi']-crd['ihi'],crd['glo']-crd['ilo'],crd['ghi']-crd['rhi'],crd['glo']-crd['rlo']
 gidrop,grdrop=gihi-gilo,grhi-grlo
 
@@ -20,6 +21,25 @@ matplotlib.rcParams['font.size']=14
 
 yticks_minor=np.arange(-1.25,1.75,0.5)
 xticks_minor=np.arange(-1.5,2.5,1)
+
+fig=plt.figure(1)
+fig.clf()
+plt.clf()
+ax1=fig.add_subplot(1,1,1)
+pos1 = ax1.get_position() # get the original position 
+pos2 = [pos1.x0, pos1.y0 + 0.03,  pos1.width, pos1.height] 
+ax1.set_position(pos2)
+ax1.tick_params(which='major',length=9,width=3,labelsize=17)
+ax1.tick_params(which='minor',length=5,width=2,labelsize=17)
+ax1.scatter(gdrop,idrop,color='k',edgecolor='None',s=6)
+ax1.scatter(gdrop[np.abs(gdrop)>1],idrop[np.abs(gdrop)>1],color='r',edgecolor='None',s=6)
+ax1.set_xlim(-2.4,2.4)
+ax1.set_ylim(-1.4,1.4)
+ax1.set_xlabel(r'$\Delta g$',fontsize=20)
+ax1.set_ylabel(r'$\Delta i$',fontsize=20)
+ax1.set_xticks(xticks_minor,minor=True)
+ax1.set_yticks(yticks_minor,minor=True)
+fig.savefig('/home/rumbaugh/var_database/Y3A1/plots/g_vs_i.EVQs.png')
 
 fig=plt.figure(1)
 fig.clf()
