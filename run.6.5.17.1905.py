cc9f2bb8dbc6bee614e0f1a7d6903df7e0747347
Nick Rumbaugh <takkyon13@gmail.com>
2017-06-06 09:36:31 -0700

    vingilot - Tue Jun  6 09:36:31 PDT 2017

--- run.6.5.17.1905.py
+++ run.6.5.17.1905.py
@@ -1,5 +1,6 @@
 import numpy as np
 from qso_fit_fix import qso_fit
+import matplotlib.pyplot as plt
 
 mcdict={'names':('DBID','RA','DEC','SDR5ID','Mi','Micorr','redshift','massBH','Lbol','u','g','r','i','z','Au'),'formats':('i8','f8','f8','i8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8')}
 delims=(8,11,11,6,8,8,7,6,7,7,7,7,7,7,7)
@@ -14,23 +15,46 @@ cr82m=np.genfromtxt(fname,dtype=mdict,delimiter=delims)
 drwname='/home/rumbaugh/s82drw_g.dat'
 drwdict={'names':('SDR5ID','ra','dec','redshift','M_i','mass_BH','chi2_pdf','ltau','lsig','ltau_lim_lo','ltau_lim_hi','lsig_lim_lo','lsig_lim_hi','edge_flag','Plike','Pnoise','Pinf','mu','npts'),'formats':('i8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','i4','f8','f8','f8','f8','i8')}
 crdrw=np.loadtxt(drwname,dtype=drwdict)
-
-g0=np.where(crdrw['SDR5ID'][0]==crmc['SDR5ID'])[0][0]
-tau0,sig0=crdrw['ltau'][0],crdrw['lsig'][0]
-tau0lerr,tau0herr,sig0lerr,sig0herr=tau0-crdrw['ltau_lim_lo'][0],crdrw['ltau_lim_hi'][0]-tau0,sig0-crdrw['lsig_lim_lo'][0],crdrw['lsig_lim_hi'][0]-sig0
+gdrw_dict={crdrw['SDR5ID'][x]: x for x in np.arange(len(crdrw))}
 
 mcLCdict={'names':('MJD_u','mag_u','mag_u_err','MJD_g','mag_g','mag_g_err','MJD_r','mag_r','mag_r_err','MJD_i','mag_i','mag_i_err','MJD_z','mag_z','mag_z_err','ra','dec'),'formats':('f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8')}
-crlc=np.loadtxt('/home/rumbaugh/QSO_S82/%i'%(crmc['DBID'][g0]),dtype=mcLCdict)
-LCcr=np.zeros((len(crlc)*1,),dtype={'names':('DatabaseID','RA','DEC','MJD','BAND','MAG','MAGERR','FLAG'),'formats':('i8','f8','f8','f8','|S4','f8','f8','i8')})
+
+maxind=100
+taumc,sigmc,taumclerr,taumcherr,sigmclerr,sigmcherr,taub,sigb=np.zeros(maxind),np.zeros(maxind),np.zeros(maxind),np.zeros(maxind),np.zeros(maxind),np.zeros(maxind),np.zeros(maxind),np.zeros(maxind)
+
+for i in np.arange(0,maxind):
+
+    g0=np.where(crdrw['SDR5ID'][i]==crmc['SDR5ID'])[0][0]
+    tau0,sig0=crdrw['ltau'][i],crdrw['lsig'][i]
+    tau0lerr,tau0herr,sig0lerr,sig0herr=tau0-crdrw['ltau_lim_lo'][i],crdrw['ltau_lim_hi'][i]-tau0,sig0-crdrw['lsig_lim_lo'][i],crdrw['lsig_lim_hi'][i]-sig0
+    taumc[i],sigmc[i],taumclerr[i],taumcherr[i],sigmclerr[i],sigmcherr[i]=tau0,sig0,tau0lerr,tau0herr,sig0lerr,sig0herr
+    crlc=np.loadtxt('/home/rumbaugh/QSO_S82/%i'%(crmc['DBID'][g0]),dtype=mcLCdict)
+    LCcr=np.zeros((len(crlc)*1,),dtype={'names':('DatabaseID','RA','DEC','MJD','BAND','MAG','MAGERR','FLAG'),'formats':('i8','f8','f8','f8','|S4','f8','f8','i8')})
 
 
-for b,ib in zip(['g'],np.arange(1)):
-    LCcr['MJD'][ib*len(crlc):(ib+1)*len(crlc)],LCcr['BAND'][ib*len(crlc):(ib+1)*len(crlc)],LCcr['MAG'][ib*len(crlc):(ib+1)*len(crlc)],LCcr['MAGERR'][ib*len(crlc):(ib+1)*len(crlc)],LCcr['RA'][ib*len(crlc):(ib+1)*len(crlc)],LCcr['DEC'][ib*len(crlc):(ib+1)*len(crlc)]=crlc['MJD_%s'%b],b,crlc['mag_%s'%b],crlc['mag_%s_err'%b],crlc['ra'],crlc['dec']
+    for b,ib in zip(['g'],np.arange(1)):
+        LCcr['MJD'][ib*len(crlc):(ib+1)*len(crlc)],LCcr['BAND'][ib*len(crlc):(ib+1)*len(crlc)],LCcr['MAG'][ib*len(crlc):(ib+1)*len(crlc)],LCcr['MAGERR'][ib*len(crlc):(ib+1)*len(crlc)],LCcr['RA'][ib*len(crlc):(ib+1)*len(crlc)],LCcr['DEC'][ib*len(crlc):(ib+1)*len(crlc)]=crlc['MJD_%s'%b],b,crlc['mag_%s'%b],crlc['mag_%s_err'%b],crlc['ra'],crlc['dec']
 
 
-mjd,mag,magerr=LCcr['MJD'],LCcr['MAG'],LCcr['MAGERR']
-out=qso_fit(mjd,mag,magerr,return_model=True)
-ltau=out['ltau']
-lsig=np.log10((10**ltau)*0.5*10**out['lvar'])
-print 'Butler model for %i\ntau=%f, var=%f, sig=%f'%(crmc['DBID'][0],out['ltau'],out['lvar'],lsig)
-print 'Macleod model for %i\ntau=%f -%f/+%f\nsigma=%f -%f/+%f'%(crmc['DBID'][0],tau0,tau0lerr,tau0herr,sig0,sig0lerr,sig0herr)
+    mjd,mag,magerr=LCcr['MJD'],LCcr['MAG'],LCcr['MAGERR']
+    out=qso_fit(mjd,mag,magerr,return_model=True)
+    ltau=out['ltau']
+    lsig=np.log10((10**ltau)*0.5*10**out['lvar'])
+    taub[i],sigb[i]=ltau,lsig
+    print 'Butler model for %i\ntau=%f, var=%f, sig=%f'%(crmc['DBID'][i],out['ltau'],out['lvar'],lsig)
+    print 'Macleod model for %i\ntau=%f -%f/+%f\nsigma=%f -%f/+%f'%(crmc['DBID'][i],tau0,tau0lerr,tau0herr,sig0,sig0lerr,sig0herr)
+
+plt.figure(1)
+plt.clf()
+plt.scatter(taub,sigb,s=1)
+plt.scatter(taumc,sigmc,s=1)
+xlim=plt.xlim()
+ylim=plt.ylim()
+plt.plot([xlim[0],xlim[1]],[ylim[0],ylim[1]],lw=2,ls='dashed',color='k')
+plt.scatter(taub,sigb,s=30,color='b')
+plt.errorbar(taumc,sigmc,xerr=[taumclerr,taumcherr],yerr=[sigmclerr,sigmcherr],color='r',fmt='ro',lw=2,capsize=3,mew=1)
+plt.xlim(xlim[0],xlim[1])
+plt.ylim(ylim[0],ylim[1])
+plt.xlabel('log(tau)')
+plt.ylabel('log(sigma)')
+plt.savefig('/home/rumbaugh/DRW_butler_Macleod_comptest.png')
