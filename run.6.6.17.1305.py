bd53a034023e76f39591e352077c791fd67f0dae
Nick Rumbaugh <takkyon13@gmail.com>
2017-06-06 14:02:28 -0700

    vingilot - Tue Jun  6 14:02:28 PDT 2017

--- run.6.6.17.1305.py
+++ run.6.6.17.1305.py
@@ -110,8 +110,8 @@ plt.clf()
 go=np.where((taumc!=5)&(taumc!=7.5)&(taumc!=-10))[0]
 plt.scatter(sigb[go],sigmc[go],color='r',s=2,edgecolor='None',facecolor='r')
 
-go=np.where((sigb[gevq]!=5)&(sigmc[gevq]!=7.5)&(sigmc[gevq]!=-10))[0]
-plt.scatter(taub[gevq[go]],taumc[gevq[go]],color='cyan',s=8,edgecolor='None',facecolor='cyan')
+go=np.where((taub[gevq]!=5)&(taumc[gevq]!=7.5)&(taumc[gevq]!=-10))[0]
+plt.scatter(sigb[gevq[go]],sigmc[gevq[go]],color='cyan',s=8,edgecolor='None',facecolor='cyan')
 xlim=plt.xlim()
 ylim=plt.ylim()
 plt.plot(np.full(2,np.min([xlim[0],xlim[1]])),np.full(2,np.max([ylim[0],ylim[1]])),lw=2,ls='dashed',color='k')
