4b7bead247cacd921d2251a7e5c38995dd839500
Nick Rumbaugh <takkyon13@gmail.com>
2017-06-05 18:04:09 -0700

    vingilot - Mon Jun  5 18:04:09 PDT 2017

--- run.6.5.17.1905.py
+++ run.6.5.17.1905.py
@@ -3,7 +3,7 @@ from qso_fit_fix import qso_fit
 
 mcdict={'names':('DBID','RA','DEC','SDR5ID','Mi','Micorr','redshift','massBH','Lbol','u','g','r','i','z','Au'),'formats':('i8','f8','f8','i8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8')}
 delims=(8,11,11,6,8,8,7,6,7,7,7,7,7,7,7)
-crmc=np.genfromtxt('/home/rumbaugh/macleodQSOs/DB_QSO_S82.dat',dtype=mcdict,delimiter=delims)
+crmc=np.genfromtxt('/home/rumbaugh/macleodQSOs/DB_QSO_S82.dat',dtype=mcdict)#,delimiter=delims)
 
 
 fname='/home/rumbaugh/master_QSO_S82.dat'
