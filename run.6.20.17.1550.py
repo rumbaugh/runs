17a258edfa2b92fe24d0b71d8ccc8b452f0a7f87
Nick Rumbaugh <takkyon13@gmail.com>
2017-06-20 19:05:44 -0700

    vingilot - Tue Jun 20 19:05:44 PDT 2017

--- run.6.20.17.1550.py
+++ run.6.20.17.1550.py
@@ -44,8 +44,9 @@ df['group']=pd.cut(df.ltaudiff,[-np.inf,-0.6,-0.2,0.2,2,np.inf],labels=['D','C',
 coldict={'A':'blue','B':'green','C':'orange','D':'magenta','Z':'red'}
 
 grand=np.zeros(0,dtype='i8')
-for group in ['A','B','C','D','Z']:
+for group in ['A','B','C','D']:
     grand=np.append(grand,np.random.choice(df.index[df.group==group],numrands,replace=False))
+    grand=np.append(grand,np.random.choice(df.index[df.group=='Z'],5,replace=False))
 for i,DBID in zip(grand,df.DBID.values[grand]):
     sample=pickle.load(open("/home/rumbaugh/CARpickles/{}.DRWsample.pickle".format(DBID),'rb'))
     sample.plot_2dkde('log_omega','sigma',doPlotStragglers=False)
