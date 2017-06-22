ad768758f02870bdd821a4fa0e480ea0b4fd8c91
rumbaugh <takkyon13@gmail.com>
2017-06-21 22:50:10 -0500

    formenos - Wed Jun 21 22:50:10 CDT 2017

--- run.6.20.17.1150.py
+++ run.6.20.17.1150.py
@@ -3,7 +3,7 @@ PCA0=SDSS_PCA.SDSS_PCA(masterfile="/home/rumbaugh/random_SDSS_specs.csv")
 PCA0.cut_master(100)
 PCA0.load_spec_files('/home/rumbaugh/spec_randSDSS',wavstep=20,savefile='/home/rumbaugh/SDSS_PCA_fluxdf.csv')
 PCA0.DoPCA(n_components=10)
-PCA0.KDTreeClassify()
+PCA0.NNClassify()
 print 'predicted_y: ',PCA0.predicted_y
 PCA0.ComparePredictions()
 PCA0.ComparePredictions(verbose=True)
