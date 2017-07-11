996796baab5395276132b970497cbbedf63a13c0
Nick Rumbaugh <takkyon13@gmail.com>
2017-07-10 19:05:04 -0700

    vingilot - Mon Jul 10 19:05:04 PDT 2017

--- run.7.10.17.2040.py
+++ run.7.10.17.2040.py
@@ -124,7 +124,7 @@ try:
 except NameError:
     sm_dict={}
 
-bounds_dict={'sig^2':[0.,0.1],'sig^2(Mac)':[0,.1],'tau':[0,1200],'tau(Mac)':[0,1200]}
+bounds_dict={'sig^2':[0.,0.05],'sig^2(Mac)':[0,.05],'tau':[0,1200],'tau(Mac)':[0,1200]}
 
 for mapper,mappername in zip([fulldf.sigma.values,fulldf.sigma_mac.values,fulldf.tau.values/(1.+fulldf.REDSHIFT.values),(10**fulldf.ltau.values)/(1.+fulldf.REDSHIFT.values)],['sig^2','sig^2(Mac)','tau','tau(Mac)']):
 
