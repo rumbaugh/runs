c8ceefdcae49b2f7ab6d2cc3dcd84be5e166b127
Nick Rumbaugh <takkyon13@gmail.com>
2017-07-14 12:06:58 -0700

    vingilot - Fri Jul 14 12:06:58 PDT 2017

--- run.7.14.17.1050.py
+++ run.7.14.17.1050.py
@@ -42,6 +42,7 @@ plt.clf()
 execfile('/home/rumbaugh/pythonscripts/set_plt_params.py')
 for objclass in ['STAR','QSO','GALAXY']:
     tmpdf=fulldf[fulldf['class'].values==objclass]
-    plt.scatter(tmpdf.sigma.values,tmpdf.tau.values,color=cdict[objclass],label=objclass,s=8,edgecolor='None',alpha=0.3)
+    plt.scatter(np.log10(tmpdf.sigma.values),tmpdf.tau.values,color=cdict[objclass],label=objclass,s=8,edgecolor='None',alpha=0.5)
+plt.xlim(-0.01,.15)
 plt.legend()
-plt.savefig('/home/rumbaugh.specplot_SNfields_S2.cen_30122.png')
+plt.savefig('/home/rumbaugh/specplot_SNfields_S2.cen_30122.png')
