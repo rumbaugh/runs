071251a41d4fa8fadcbcfda4f5908af6f36e596e
Nick Rumbaugh <takkyon13@gmail.com>
2017-07-14 12:07:48 -0700

    vingilot - Fri Jul 14 12:07:48 PDT 2017

--- run.7.14.17.1050.py
+++ run.7.14.17.1050.py
@@ -43,6 +43,6 @@ execfile('/home/rumbaugh/pythonscripts/set_plt_params.py')
 for objclass in ['STAR','QSO','GALAXY']:
     tmpdf=fulldf[fulldf['class'].values==objclass]
     plt.scatter(np.log10(tmpdf.sigma.values),tmpdf.tau.values,color=cdict[objclass],label=objclass,s=8,edgecolor='None',alpha=0.5)
-plt.xlim(-0.01,.15)
+#plt.xlim(-0.01,.15)
 plt.legend()
 plt.savefig('/home/rumbaugh/specplot_SNfields_S2.cen_30122.png')
