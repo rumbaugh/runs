b60698dd1f9536cd2a34b61a80bfb430324beb05
Nick Rumbaugh <takkyon13@gmail.com>
2017-07-19 13:41:34 -0700

    vingilot - Wed Jul 19 13:41:34 PDT 2017

--- run.7.19.17.1520.py
+++ run.7.19.17.1520.py
@@ -6,10 +6,13 @@ specdf=pd.read_csv('/home/rumbaugh/SNfields_S2_cen_30122_spec.csv')
 
 fitdf=pd.read_csv('/home/rumbaugh/SN_fields.S2.cen_{}.CAR1fits.csv'.format(ri))
 
+autormsdf=pd.read_csv('/home/rumbaugh/SN_fields.S2.cen_{}.AUTO_RMS.csv'.format(ri))
+
 rmsdf=pd.read_csv('/home/rumbaugh/SN_fields.S2.cen_{}.RMS.csv'.format(ri))
 
 fulldf=pd.merge(fitdf,specdf,left_on=['DataID','cid'],right_on=['dataid','coadd_object_id'],suffixes=('_DR7','_DR13'))
 fulldf=pd.merge(fulldf,rmsdf,on=['DataID','cid'],suffixes=('','_RMS'))
+fulldf=pd.merge(fulldf,autofitdf,on=['DataID','cid'],suffixes=('','_AUTO'))
 fulldf=fulldf[fulldf.numepoch.values>5]
 
 try:
@@ -20,6 +23,16 @@ except AttributeError:
     fulldf['sigma_err']=0.5*np.sqrt(4*(fulldf.sigma_hat.values*fulldf.tau.values)**2*(0.5*(fulldf.sigub.values-fulldf.siglb.values))**2+fulldf.sigma_hat.values**4*(0.5*(fulldf.tauub.values-fulldf.taulb.values))**2)
 
 cdict={x:y for x,y in zip(['STAR','QSO','GALAXY'],['red','cyan','green'])}
+
+plt.figure(1)
+plt.clf()
+execfile('/home/rumbaugh/pythonscripts/set_plt_params.py')
+for objclass in ['STAR','QSO','GALAXY']:
+    tmpdf=fulldf[fulldf['class'].values==objclass]
+    plt.hist(tmpdf.RMS.values,color=cdict[objclass],alpha=0.4)
+plt.xlabel('RMS variability (mags)')
+plt.ylabel('Number of objects')
+plt.savefig('/home/rumbaugh/RMS_hist.SN_fields.S2.cen_{}.RMS.png'.format(ri))
 
 plt.figure(1)
 plt.clf()
