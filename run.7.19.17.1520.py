6825d16b53ce9d32b77f5f396a690df940e3dc74
Nick Rumbaugh <takkyon13@gmail.com>
2017-07-19 14:37:03 -0700

    vingilot - Wed Jul 19 14:37:03 PDT 2017

--- run.7.19.17.1520.py
+++ run.7.19.17.1520.py
@@ -2,9 +2,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+ri=30122
 specdf=pd.read_csv('/home/rumbaugh/SNfields_S2_cen_30122_spec.csv')
 
 fitdf=pd.read_csv('/home/rumbaugh/SN_fields.S2.cen_{}.CAR1fits.csv'.format(ri))
+
+autofitdf=pd.read_csv('/home/rumbaugh/SN_fields.S2.cen_{}.AUTO_CAR1fits.csv'.format(ri))
 
 autormsdf=pd.read_csv('/home/rumbaugh/SN_fields.S2.cen_{}.AUTO_RMS.csv'.format(ri))
 
@@ -13,6 +16,7 @@ rmsdf=pd.read_csv('/home/rumbaugh/SN_fields.S2.cen_{}.RMS.csv'.format(ri))
 fulldf=pd.merge(fitdf,specdf,left_on=['DataID','cid'],right_on=['dataid','coadd_object_id'],suffixes=('_DR7','_DR13'))
 fulldf=pd.merge(fulldf,rmsdf,on=['DataID','cid'],suffixes=('','_RMS'))
 fulldf=pd.merge(fulldf,autormsdf,on=['DataID','cid'],suffixes=('','_AUTO'))
+fulldf=pd.merge(fulldf,autofitdf,on=['DataID','cid'],suffixes=('','_AUTO'))
 fulldf=fulldf[fulldf.numepoch.values>5]
 
 try:
