ee21c950d0058a137251e12264179fc1e86d2ae4
Nick Rumbaugh <takkyon13@gmail.com>
2017-07-19 14:41:32 -0700

    vingilot - Wed Jul 19 14:41:32 PDT 2017

--- run.7.19.17.1520.py
+++ run.7.19.17.1520.py
@@ -37,9 +37,10 @@ plt.clf()
 execfile('/home/rumbaugh/pythonscripts/set_plt_params.py')
 for objclass in ['STAR','QSO','GALAXY']:
     tmpdf=fulldf[fulldf['class'].values==objclass]
-    plt.hist(tmpdf.RMS.values,color=cdict[objclass],alpha=0.4,range=(0,0.6),bins=20)
+    plt.hist(tmpdf.RMS.values,color=cdict[objclass],alpha=0.4,range=(0,0.6),bins=20,label=objclass)
 plt.xlabel('RMS variability (mags)')
 plt.ylabel('Number of objects')
+plt.legend(loc='upper right',frameon=False)
 plt.savefig('/home/rumbaugh/RMS_hist.SN_fields.S2.cen_{}.RMS.png'.format(ri))
 
 plt.figure(1)
@@ -47,9 +48,10 @@ plt.clf()
 execfile('/home/rumbaugh/pythonscripts/set_plt_params.py')
 for objclass in ['STAR','QSO','GALAXY']:
     tmpdf=fulldf[fulldf['class'].values==objclass]
-    plt.hist(tmpdf.RMS_AUTO.values,color=cdict[objclass],alpha=0.4,range=(0,0.6),bins=20)
+    plt.hist(tmpdf.RMS_AUTO.values,color=cdict[objclass],alpha=0.4,range=(0,0.6),bins=20,label=objclass)
 plt.xlabel('RMS variability (mags)')
 plt.ylabel('Number of objects')
+plt.legend(loc='upper right',frameon=False)
 plt.savefig('/home/rumbaugh/RMS_hist_AUTO.SN_fields.S2.cen_{}.RMS.png'.format(ri))
 
 
