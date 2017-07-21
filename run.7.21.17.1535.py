beda29b333dc37a6a8ad9ca40351813fb7025e1e
Nick Rumbaugh <takkyon13@gmail.com>
2017-07-21 13:38:53 -0700

    vingilot - Fri Jul 21 13:38:53 PDT 2017

---
+++ run.7.21.17.1535.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import pickle
+import pyfits as py
+
+for field in ['S1','S2']:
+
+    hdu=py.open('/home/rumbaugh/{}_lc.fits'.format(field))
+    data=hdu[1].data
+
+    outdf=pd.DataFrame({x: data[x] for x in ['COADD_OBJECT_ID','RA','DEC']})
+    outdf['DataID']=outdf.index.values
+    outdf['numrow']=np.arange(len(gri))
+
+    outdf.to_csv('/home/rumbaugh/SN_fields.{}.radecid.csv'.format(field),index=False)
