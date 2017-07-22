9dfe1becf601a9301af61be9e4b4e0bf167b8f21
Nick Rumbaugh <takkyon13@gmail.com>
2017-07-22 15:17:35 -0700

    vingilot - Sat Jul 22 15:17:34 PDT 2017

--- run.7.21.17.1730.py
+++ run.7.21.17.1730.py
@@ -9,16 +9,16 @@ df2['field']='S2'
 df=df1.append(df2)
 
 try:
-    fulldf.sigma_hat
+    df.sigma_hat
 except AttributeError:
-    fulldf['sigma_hat']=fulldf.sig.values
-    fulldf['sigma']=0.5*(fulldf.sigma_hat.values**2)*fulldf.tau.values
-    fulldf['sigma_err']=0.5*np.sqrt(4*(fulldf.sigma_hat.values*fulldf.tau.values)**2*(0.5*(fulldf.sigub.values-fulldf.siglb.values))**2+fulldf.sigma_hat.values**4*(0.5*(fulldf.tauub.values-fulldf.taulb.values))**2)
+    df['sigma_hat']=df.sig.values
+    df['sigma']=0.5*(df.sigma_hat.values**2)*df.tau.values
+    df['sigma_err']=0.5*np.sqrt(4*(df.sigma_hat.values*df.tau.values)**2*(0.5*(df.sigub.values-df.siglb.values))**2+df.sigma_hat.values**4*(0.5*(df.tauub.values-df.taulb.values))**2)
 
 
 plt.figure(1)
 plt.clf()
-plt.errorbar(fulldf.tau.values,fulldf.sigma.values,xerr=[fulldf.tau.values-fulldf.taulb.values,fulldf.tauub.values-fulldf.tau.values],yerr=df.sigma_err.values,fmt='o',capsize=2,mew=0,ms=3,alpha=0.5)
+plt.errorbar(df.tau.values,df.sigma.values,xerr=[df.tau.values-df.taulb.values,df.tauub.values-df.tau.values],yerr=df.sigma_err.values,fmt='o',capsize=2,mew=0,ms=3,alpha=0.5)
 plt.xlabel(r'$\sigma**2$')
 plt.ylabel(r'$\tau$ (days)')
 
@@ -26,9 +26,9 @@ plt.savefig('/home/rumbaugh/SN_fields.tau_vs_sigma.AUTO.CAR1fits.png')
 
 plt.figure(1)
 plt.clf()
-plt.hist(fulldf.sigma.values,color='k',alpha=0.5,label='All')
-plt.hist(fulldf[fulldf.field.values=='S1'].sigma.values,color='r',alpha=0.5,label='S1')
-plt.hist(fulldf[fulldf.field.values=='S2'].sigma.values,color='b',alpha=0.5,label='S2')
+plt.hist(df.sigma.values,color='k',alpha=0.5,label='All')
+plt.hist(df[df.field.values=='S1'].sigma.values,color='r',alpha=0.5,label='S1')
+plt.hist(df[df.field.values=='S2'].sigma.values,color='b',alpha=0.5,label='S2')
 plt.xlabel(r'$\sigma**2$')
 plt.ylabel(r'$\tau$ (days)')
 plt.legend()
