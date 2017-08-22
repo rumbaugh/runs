eaa57dafe71c8c4a63ef2320019a1674c110adbd
Nick Rumbaugh <takkyon13@gmail.com>
2017-08-22 14:31:33 -0700

    vingilot - Tue Aug 22 14:31:33 PDT 2017

--- run.8.22.17.1550.py
+++ run.8.22.17.1550.py
@@ -2,6 +2,7 @@ import numpy as np
 import pyfits as py
 import pandas as pd
 
+DBdir='/home/rumbaugh/var_database/Y3A1'
 crd=np.loadtxt('/home/rumbaugh/var_database/Y3A1/DR7_full_magdiffs_wDBID.4.28.17.tab',dtype={'names':('RA','DEC','z','mjdlo','glo','siglo','flaglo','mjdhi','ghi','sighi','flaghi','RA_DES','DEC_DES','DBID'),'formats':('f8','f8','f8','f8','f8','f8','i8','f8','f8','f8','i8','f8','f8','|S24')})
 drops=np.abs(crd['glo']-crd['ghi'])
 
@@ -20,3 +21,16 @@ DR7ID=np.array([SN2DR7[x] for x in sdssname],dtype='i8')
 gsort=np.argsort(DR7ID)
 DR7ID,sdssname,crd=DR7ID[gsort],sdssname[gsort],crd[gsort]
 
+for i in range(0,len(DR7ID)):
+    DBID=crd['DBID'][i]
+    
+    cr=np.loadtxt('%s/%s/LC.tab'%(DBdir,DBID),dtype={'names':('DatabaseID','Survey','SurveyCoaddID','SurveyObjectID','RA','DEC','MJD','TAG','BAND','MAGTYPE','MAG','MAGERR','FLAG'),'formats':('|S64','|S20','|S20','|S20','f8','f8','f8','|S20','|S12','|S12','f8','f8','i8')},skiprows=1)
+    crout=np.loadtxt('%s/%s/outliers.tab'%(DBdir,DBID),dtype='i8')
+    try:
+        crmac=np.loadtxt('%s/%s/Macleod_LC.tab'%(DBdir,DBID),dtype={'names':('DatabaseID','RA','DEC','MJD','BAND','MAG','MAGERR','FLAG'),'formats':('i8','f8','f8','f8','|S4','f8','f8','i8')})
+    except IOError:
+        print 'No Mac for %s'%DBID
+    try:
+        croutmac=np.loadtxt('%s/%s/outliers_Macleod.tab'%(DBdir,DBID),dtype='i8')
+    except IOError:
+        print 'No Mac outliers for %s'%DBID
