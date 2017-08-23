f8a93e79eeba2740e7cdef23d265c7a2fedc45a7
Nick Rumbaugh <takkyon13@gmail.com>
2017-08-22 22:48:00 -0700

    vingilot - Tue Aug 22 22:48:00 PDT 2017

--- run.8.22.17.1650.py
+++ run.8.22.17.1650.py
@@ -56,7 +56,7 @@ for i in range(0,3):
     except IOError:
         crmac=None
     if crmac!=None: 
-        tmpcrmac=np.zeros(len(crmac),dtype={'names':('DatabaseID','Survey','MJD','BAND','MAG','MAGERR','FLAG'),'formats':('i8','|S20','f8','|S4','f8','f8','i8')})
+        tmpcrmac=np.zeros(len(crmac),dtype={'names':('DatabaseID','Survey','MJD','BAND','MAG','MAGERR','FLAG'),'formats':('i8','|S20','f8','|S12','f8','f8','i8')})
         tmpcrmac['DatabaseID'],tmpcrmac['Survey'],tmpcrmac['MJD'],tmpcrmac['BAND'],tmpcrmac['MAG'],tmpcrmac['MAGERR'],tmpcrmac['FLAG']=crmac['DatabaseID'],np.full(len(crmac),"SDSS",dtype='|S20'),crmac['MJD'],crmac['BAND'],crmac['MAG'],crmac['MAGERR'],crmac['FLAG']
         cr=np.append(cr,crmac)
         crout=np.append(crout,macflags)
